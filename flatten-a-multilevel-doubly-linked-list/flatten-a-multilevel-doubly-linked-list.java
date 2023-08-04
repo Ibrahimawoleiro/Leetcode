2f1a02135ac5ff79b27b32d03f924b34b955ee76
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2023-08-04 09:02:57 -0400

    Time: 0 ms, Memory: 41 MB - LeetHub

---
+++ flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.java
@@ -0,0 +1,33 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
+class Solution {
+    public Node flatten(Node head) {
+        Stack<Node> stack = new Stack<Node>();
+        Node temp = head;
+        Node previous=head;
+        Node preventNullHead=head;
+        while(head!= null || !stack.isEmpty()){
+            if(head.child!=null){
+                if(head.next!=null){
+                stack.push(head.next);
+                }
+                head.next=head.child;
+                head.next.prev=head;
+                head.child=null;
+            }else if(head.next==null && !stack.isEmpty()){
+                head.next=stack.pop();
+                head.next.prev=head;
+            }
+            head=head.next;
+        }
+        return temp;
+    }
+}
