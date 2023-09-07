4a2f37f774c76a9e4343cf1695b6fcae8d4cfdfb
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2023-09-07 10:41:43 -0400

    Time: 231 ms, Memory: 16.5 MB - LeetHub

--- next-greater-element-i/next-greater-element-i.py
+++ next-greater-element-i/next-greater-element-i.py
@@ -2,15 +2,15 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         ans = []
         for num in range(len(nums1)):
-            index = -1
+            right_greater = -1
             seen = False
             for number in range(len(nums2)):
                 if nums2[number] == nums1[num]:
                     seen = True
                     continue
                 if nums2[number] > nums1[num] and seen:
-                    index = nums2[number]
+                    right_greater = nums2[number]
                     break
-            ans.append(index)
+            ans.append(right_greater)
         return ans
         
