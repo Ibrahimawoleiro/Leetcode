87e6caff662fd54dc407d2d04ae52a15bf124224
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2023-09-13 21:48:37 -0400

    Time: 37 ms, Memory: 16.3 MB - LeetHub

--- maximum-69-number/maximum-69-number.py
+++ maximum-69-number/maximum-69-number.py
@@ -3,10 +3,8 @@ class Solution:
         num_to_string = str(num)
         
         for digit in range(len(num_to_string)):
-            print(num_to_string[digit], "g")
             if num_to_string[digit] == "6":
                 k=num_to_string.replace("6","9",1)
-                print(k)
                 ans = int(k)
                 return ans
             
