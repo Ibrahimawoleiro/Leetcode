c6f1d8e27ac3b837a6cfa83a1fb5c9b785a1f97d
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2023-10-24 02:59:58 -0400

    Time: 267 ms, Memory: 18.6 MB - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -8,7 +8,7 @@ class Solution:
         for array_index in range(len(grid)):
             for number_index in range(len(grid[array_index])):
                 if grid[array_index][number_index] == "1":
-                    print("opkjkl")
+                
                     island_count +=1
                     grid[array_index][number_index] = "5"
                     if array_index-1>=0 and grid[array_index-1][number_index]=="1":
@@ -21,9 +21,9 @@ class Solution:
                         Queue.append([array_index,number_index+1])
              
                     while Queue:
-                        print(Queue,"hjknlm")
+                   
                         curr = Queue.popleft()
-                        print(Queue)
+                     
                         grid[curr[0]][curr[1]]="5"
                         if curr[0] - 1 >= 0 and grid[curr[0]-1][curr[1]]=="1":
                             Queue.append([curr[0]-1,curr[1]])
