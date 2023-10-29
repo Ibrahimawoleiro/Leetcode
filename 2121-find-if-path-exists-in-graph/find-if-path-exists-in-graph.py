a9f87cd4b4052706e25ae72e03e477d65319e93e
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2023-10-29 14:28:29 -0400

    Time: 2013 ms (11.46%) | Memory: 109 MB (46.72%) - LeetSync

---
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -0,0 +1,44 @@
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        dictionary={}
+        for connection in edges:
+            print(connection)
+            if connection[0] not in dictionary:
+                if connection[1] not in dictionary:
+                        dictionary[connection[1]]=[connection[0]]
+                        dictionary[connection[0]]=[connection[1]]
+                elif connection[1] in dictionary:
+                    dictionary[connection[0]]=[connection[1]]
+                    dictionary[connection[1]].append(connection[0])
+            elif connection[1] not in dictionary:
+                if connection[0] not in dictionary:
+                    dictionary[connection[1]]=[connection[0]]
+                    dictionary[connection[0]]=[connection[1]]
+                elif connection[0] in dictionary:
+                    dictionary[connection[1]]=[connection[0]]
+                    dictionary[connection[0]].append(connection[1])
+            elif connection[0] in dictionary and connection[1] in dictionary:
+                dictionary[connection[1]].append(connection[0])
+                dictionary[connection[0]].append(connection[1])
+
+        seen = set()
+        isVisited = set()
+        direction = []
+
+        direction.append(source)
+        while(direction):
+            print(direction)
+            curr = direction.pop()
+            
+            print(curr)
+            if curr == destination:
+                return True
+            if curr not in isVisited:
+                isVisited.add(curr)
+                for neighbor in dictionary[curr]:
+                    if neighbor not in seen:
+                        direction.append(neighbor)
+                        seen.add(neighbor)
+        
+
+        return False
