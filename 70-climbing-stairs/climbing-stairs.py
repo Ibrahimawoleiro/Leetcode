ab9130463f6e005b47df4c4369bd3b84c9d6b25d
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2023-12-02 10:54:33 -0500

    Time: 38 ms (52.05%) | Memory: 16.5 MB (10.81%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,28 +1,20 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n==0:
+        if n==1:
             return 1
+        if n==2:
+            return 2
 
-        ways = 0
+        arr = [0 for number in range(n)]
 
-        dictionary={
-            n:1,
-            n-1:1
-        }
-     
-        return  self.helper(dictionary,0,ways,n,n-1)
-     
-    def helper(self, dictionary,beginning,ways,max,second_max):
-        if beginning == max or beginning == second_max:
-            return 1
-        elif beginning in dictionary:
-            return dictionary[beginning]
-        if(beginning+1 <= max):
-            ways += self.helper(dictionary,beginning+1,ways,max,second_max)
-        
-        if(beginning + 2 <= max):
-            ways += self.helper(dictionary,beginning+2,ways,max,second_max)
+        arr[0] = 1
 
-        if beginning not in dictionary:
-            dictionary[beginning]= ways
-        return ways
+        arr[1] = 2
+  
+        for num in range(n):
+            print(arr, num)
+            if num == 0 or num == 1:
+                continue
+
+            arr[num] = arr[num-1] + arr[num-2]
+        return arr[n-1]
