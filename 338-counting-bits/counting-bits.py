bcefbda44ab010ec46f0b6eca6cb82651488724d
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2023-12-07 18:44:11 -0500

    Time: 86 ms (36.65%) | Memory: 23.2 MB (15.26%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,18 +1,32 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        #Approach1
-        arr = [0 for num in range(n+1)]
+        # #Approach1
+        # arr = [0 for num in range(n+1)]
 
-        for val in range(len(arr)):
+        # for val in range(len(arr)):
            
-            temp = val
-            one_count = 0
-            while(temp > 0):
-                if(temp % 2 ==1):
-                    one_count+=1
-                temp= int(temp/2)
-                print(temp)
+        #     temp = val
+        #     one_count = 0
+        #     while(temp > 0):
+        #         if(temp % 2 ==1):
+        #             one_count+=1
+        #         temp= int(temp/2)
             
-            arr[val] = one_count
+        #     arr[val] = one_count
 
+        # return arr
+
+        #Approach2
+        arr = [0 for num in range(n+1)]
+        time_to_change = 2
+        checker = 0
+        for val in range(n+1):
+            if val == 0 or val == 1:
+                arr[val] = val
+                continue
+            if val == time_to_change:
+                time_to_change*=2
+                checker+=1 
+            arr[val] = 1 + arr[val - 2**checker]
+        
         return arr
