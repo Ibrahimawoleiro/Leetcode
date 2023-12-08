8e0f5e7c5d1f53322338feb0fdfcfd203d87658d
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2023-12-08 18:34:27 -0500

    Time: 588 ms (38.73%) | Memory: 49.3 MB (18.65%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -5,18 +5,41 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        #Approach1
-        arr = []
-        while head != None:
-            arr.append(head.val)
-            head = head.next
+        # #Approach1
+        # arr = []
+        # while head != None:
+        #     arr.append(head.val)
+        #     head = head.next
         
-        front_pointer = 0
-        back_pointer = len(arr) - 1
-        while(front_pointer <= back_pointer):
-            if(arr[front_pointer] != arr[back_pointer]):
-                return False
-            front_pointer += 1
-            back_pointer -= 1
+        # front_pointer = 0
+        # back_pointer = len(arr) - 1
+        # while(front_pointer <= back_pointer):
+        #     if(arr[front_pointer] != arr[back_pointer]):
+        #         return False
+        #     front_pointer += 1
+        #     back_pointer -= 1
         
+        # return True
+
+        #Approach2
+        slow = head
+        fast = head
+        arr=[]
+        while(fast != None and fast.next != None):
+            arr.append(slow.val)
+            slow = slow.next
+            fast = fast.next.next
+        if fast == None:
+            while(slow != None):
+                if(slow.val != arr.pop()):
+                    return False
+                slow = slow.next
+        elif fast.next == None:
+            slow = slow.next
+            while(slow != None):
+                if(slow.val != arr.pop()):
+                    return False
+                slow = slow.next
         return True
+            
+            
