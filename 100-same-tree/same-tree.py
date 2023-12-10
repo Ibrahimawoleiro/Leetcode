b76ab2ab359a7324456ba5c3cb18ab8d5103f2ce
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2023-12-10 04:51:53 -0500

    Time: 43 ms (22.07%) | Memory: 16.4 MB (35.38%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -5,22 +5,39 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    #     #Approach1
+    #def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+    #     arr1 = []
+    #     arr2 = []
+
+    #     self.helper(p, arr1,'beginning',0)
+    #     self.helper(q, arr2, 'beginning',0)
+
+    #     return arr1 == arr2
+
+    # def helper(self,root, arr, direction, count):
+    #     if root == None:
+    #         return 
+    #     arr.append(str(root.val)+direction+str(count))
+    #     if root.left:
+    #         self.helper(root.left, arr, 'l', count+1)
+    #     if root.right:
+    #         self.helper(root.right,arr, 'r',count+1)
+
+    #Approach2
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        #Approach1
-        arr1 = []
-        arr2 = []
+        return self.helper(p,q)
 
-        self.helper(p, arr1,'beginning',0)
-        self.helper(q, arr2, 'beginning',0)
+    def helper(self,root1, root2):
+        if not root1 and not root2:
+            return True
+        if not root1 and root2 or root1 and not root2:
+            return False
+        if root1.val != root2.val:
+            return False
 
-        return arr1 == arr2
+        r =self.helper(root1.right, root2.right)
+        l =self.helper(root1.left, root2.left)
 
-    def helper(self,root, arr, direction, count):
-        if root == None:
-            return 
-        arr.append(str(root.val)+direction+str(count))
-        if root.left:
-            self.helper(root.left, arr, 'l', count+1)
-        if root.right:
-            self.helper(root.right,arr, 'r',count+1)
+        return r and l 
         
