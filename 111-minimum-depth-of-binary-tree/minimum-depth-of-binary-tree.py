25402f302773b401f6ad89fa812202bf18970be1
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2023-12-10 03:42:41 -0500

    Time: 338 ms (99.50%) | Memory: 52.6 MB (81.79%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -6,20 +6,45 @@
 #         self.right = right
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
+    #     #Approach1-DFS/recursion
+    #     if root == None:
+    #         return 0
+    #     return self.helper(root)
+
+    # def helper(self, current):
+    #     if current.left == None and current.right == None:
+    #         return 1
+    #     if current.left:
+    #         l = 1 + self.helper(current.left)
+    #     if current.right:
+    #         r = 1 + self.helper(current.right)
+    #     if current.left and current.right:
+    #         return min(l,r)
+    #     elif current.left:
+    #         return l
+    #     else:
+    #         return r 
+
+        #Approach2-BFS
         if root == None:
             return 0
-        return self.helper(root)
+        count = 0
+        queue = deque()
+        queue.append(root)
+        queue.append(TreeNode(None,None,None))
+        while(queue):
+            current = queue.popleft()
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
+            if current.left == None and current.right == None and current.val!= None:
+                return count + 1
+            if current.val == None:
+                count+=1
+                if(len(queue) > 0):
+                    queue.append(current)
+        
+        return count
 
-    def helper(self, current):
-        if current.left == None and current.right == None:
-            return 1
-        if current.left:
-            l = 1 + self.helper(current.left)
-        if current.right:
-            r = 1 + self.helper(current.right)
-        if current.left and current.right:
-            return min(l,r)
-        elif current.left:
-            return l
-        else:
-            return r 
+        
