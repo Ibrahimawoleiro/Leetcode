3d05ba0de2c26a0a4318139928c7f643096ffab0
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2023-12-11 18:08:23 -0500

    Time: 138 ms (96.53%) | Memory: 17.8 MB (82.95%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,17 +1,34 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        dictionary = {}
-        for val in nums:
-            if val in dictionary:
-                dictionary[val]+=1
-                if len(nums) % 2 == 0 and dictionary[val] >= floor(len(nums) / 2):
-                    return val
-                elif dictionary[val] == floor((len(nums)+1) / 2):
-                    return val
-            else:
-                dictionary[val] = 1
+        # #Approach1
+        # if len(nums) == 1:
+        #     return nums[0]
+        # dictionary = {}
+        # for val in nums:
+        #     if val in dictionary:
+        #         dictionary[val]+=1
+        #         if len(nums) % 2 == 0 and dictionary[val] >= floor(len(nums) / 2):
+        #             return val
+        #         elif dictionary[val] == floor((len(nums)+1) / 2):
+        #             return val
+        #     else:
+        #         dictionary[val] = 1
 
-        print(dictionary)
-        return 0
+        # print(dictionary)
+        # return 0
+
+        #Approach2
+        majority = None
+        count = 0
+
+        for val in nums:
+            if count == 0:
+                majority = val
+                count = 1
+                continue
+            if val == majority:
+                count+=1
+            else:
+                count -=1
+
+        return majority
