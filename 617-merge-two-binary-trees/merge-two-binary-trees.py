b1cd9ce9c21883ed3d7d2c32e5f98a02d3cfbf82
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2023-12-11 00:36:11 -0500

    Time: 103 ms (5.02%) | Memory: 18 MB (27.92%) - LeetSync

--- 617-merge-two-binary-trees/merge-two-binary-trees.py
+++ 617-merge-two-binary-trees/merge-two-binary-trees.py
@@ -6,6 +6,11 @@
 #         self.right = right
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root1:
+            return root2
+        elif not root2:
+            return root1
+
         return self.helper(root1,root2)
 
     def helper(self, r1, r2):
