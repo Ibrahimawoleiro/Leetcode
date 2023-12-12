285f870840be2721f9fa12dc57e65fbe5a2e3bfc
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2023-12-12 01:17:56 -0500

    Time: 357 ms (5.01%) | Memory: 24.4 MB (48.59%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
@@ -1,11 +1,30 @@
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
-        result = []
-        checker = set()
-        for val in nums:
-            if val in checker:
-                result.append(val)
-                continue
-            checker.add(val)
+        # #Appraoch1
+        # result = []
+        # checker = set()
+        # for val in nums:
+        #     if val in checker:
+        #         result.append(val)
+        #         continue
+        #     checker.add(val)
 
+        # return result
+
+        #Approach2
+        in_case = None
+        result = []
+        for index in range(len(nums)):
+            if abs(nums[index]) < len(nums):
+                if (nums[abs(nums[index])]) < 0:
+                    result.append(abs(nums[index]))
+                else:
+                    nums[abs(nums[index])] *= -1
+            
+            else:
+                if not in_case:
+                    in_case = len(nums)
+                else:
+                    result.append(len(nums))
+            
         return result
