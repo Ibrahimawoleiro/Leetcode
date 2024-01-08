7c537c6cfb6570e7e32f5fdbc2e08ab760400074
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-01-08 10:17:26 -0500

    Time: 119 ms (73.93%) | Memory: 20.5 MB (7.31%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,14 +1,11 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        
+        #Approach 1
         checker = set()
-
         for val in nums:
             if val not in checker:
                 checker.add(val)
-            elif val in checker:
+            else:
                 checker.remove(val)
         
         return checker.pop()
