921a32f7431b0b2c47c49d4abe4b2c115603dd73
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-01-10 17:59:25 -0500

    Time: 2870 ms (11.92%) | Memory: 20.8 MB (34.44%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.py
+++ 303-range-sum-query-immutable/range-sum-query-immutable.py
@@ -1,47 +1,21 @@
 class NumArray:
-    #Approach1
-    # def __init__(self, nums: List[int]):
-    #     self.nums = nums
 
-    # def sumRange(self, left: int, right: int) -> int:
-    #     total = 0
-    #     while(left <= right):
-    #         total+=self.nums[left]
-    #         left += 1
-    #     return total
-    
-    # #This gives a memory limit Exceeded
-    # def __init__(self, nums: List[int]):
-    #     self.dictionary = {}
-    #     for num in range(len(nums)):
-    #         for val in range(len(nums)):
-    #             if((num,val) in self.dictionary):
-    #                 continue
-    #             elif num == val:
-    #                 self.dictionary[(num,val)] = nums[num]
-    #             else:
-    #                 curr = self.dictionary[(num,val-1)] + nums[val]
-    #                 self.dictionary[(val,num)] = curr
-    #                 self.dictionary[(num,val)] = curr
-
-    # def sumRange(self, left: int, right: int) -> int:
-    #     return self.dictionary[(left,right)]
-
-    #Approach3
     def __init__(self, nums: List[int]):
         self.nums = nums
-        for val in range(len(self.nums)):
-            if val == 0:
-                continue
-            self.nums[val] = self.nums[val - 1] + self.nums[val]
-        
-        
+
     def sumRange(self, left: int, right: int) -> int:
-        if left == 0:
-            return self.nums[right]
-        return self.nums[right] - self.nums[left - 1]
+        sum = 0
+        while(left <= right):
+            sum += self.nums[left]
+            left+=1
+        return sum
+
+
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
-# param_1 = obj.sumRange(left,right)
+#     def __init__(self, nums: List[int]):
+#         self.nums = nums
 
-        
+#     def sumRange(self, left: int, right: int) -> int:
+#         while()
+# param_1 = obj.sumRange(left,right)
