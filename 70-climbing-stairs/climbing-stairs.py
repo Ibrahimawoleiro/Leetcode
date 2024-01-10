08eeb674921f863177d85ef371c1acfce7583f52
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-01-10 14:43:17 -0500

    Time: 36 ms (68.00%) | Memory: 17.4 MB (11.62%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,59 +1,31 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         # #Approach 1
-        # if n==1:
-        #     return 1
-        # if n==2:
-        #     return 2
+        # dictionary = {
+        #     0:1,
+        #     1:1
+        # }
 
-        # arr = [0 for number in range(n)]
-
-        # arr[0] = 1
-
-        # arr[1] = 2
-  
-        # for num in range(n):
-        #     print(arr, num)
-        #     if num == 0 or num == 1:
+        # for val in range(n+1):
+        #     if val in dictionary:
         #         continue
-
-        #     arr[num] = arr[num-1] + arr[num-2]
-        # return arr[n-1]
-
-
-        # #Approach2
-        # if n==1:
-        #     return 1
-        # if n==2:
-        #     return 2
-        # dictionary = {}
-
-        # for val in range(n):
-        #     if val == 0 or val == 1:
-        #         dictionary[val+1] = val + 1
-        #     else:
-        #         dictionary[val+1] = dictionary[val] + dictionary[val - 1]
+        #     dictionary[val] = dictionary[val-1] + dictionary[val-2]
         
-        # return dictionary[n]
+        # return dictionary[n] if n in dictionary else 0
 
 
-        # Approach3
-        if n==1:
-            return 1
-        if n==2:
-            return 2
+        #Approach 2
+        arr = [0 for val in range(n+1)]
+        arr[0] = 1
+        arr[1] = 1
 
-        x = 1
-        y = 2
-
-        for val in range(n):
-            if val == 0 or val == 1: 
+        for val in range(n+1):
+            if val == 0 or val == 1:
                 continue
-            temp = x + y
-            x = y
-            y = temp
+            
+            arr[val] = arr[val - 1] + arr[val - 2]
 
-        return y
+        return arr[n]
 
 
         
