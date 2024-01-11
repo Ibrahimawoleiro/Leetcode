a06229086799573ba64d61683de1fb8cb6d0bdfd
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-01-11 17:26:14 -0500

    Time: 35 ms (98.96%) | Memory: 19.2 MB (31.91%) - LeetSync

--- 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.py
+++ 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.py
@@ -6,46 +6,29 @@
 #         self.right = right
 class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
-        # #Approach1
-        # q1 = deque()
-        # arr = []
-        # q1.append(root)
-        # while q1:
-        #     count = 0
-        #     total = 0
-        #     q2 = deque()
-        #     while(q1):
-        #         current = q1.popleft()
-        #         if current.left:
-        #             q2.append(current.left)
-        #         if current.right:
-        #             q2.append(current.right)
-        #         total+=current.val
-        #         count+=1
-        #     arr.append(total / count)
-        #     q1 = q2
-        # return arr
-
-        #Approach2
-        q1 = deque()
-        count = total = 0
-        arr = []
-        q1.append(root)
-        q1.append( TreeNode(None, None,None))
-        while(q1):
-            current = q1.popleft()
-            if current.val != None:
-                total+= current.val
+        #Approach 1
+        ans = []
+        count = 0
+        level_sum = 0
+        queue = deque()
+        queue.append(root)
+        queue.append(None)
+        while(queue):
+            current = queue.popleft()
+            if current:
+                level_sum += current.val
                 count+=1
                 if current.left:
-                    q1.append(current.left)
+                    queue.append(current.left)
                 if current.right:
-                    q1.append(current.right)
-            elif current.val == None:
-                arr.append(total/count)
-                total = 0
-                count = 0
-                if len(q1) != 0:
-                    q1.append(current)
-        
-        return arr
+                    queue.append(current.right)
+            
+            else:
+                ans.append(level_sum/count if count else 1)
+                level_sum = count = 0
+                if len(queue) > 0:
+                    queue.append(None)
+
+        return ans
+
+
