f451537ab1811ab776eae45b8af8d5b594e0e65a
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-01-11 12:55:05 -0500

    Time: 54 ms (79.20%) | Memory: 20.3 MB (15.97%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.py
+++ 203-remove-linked-list-elements/remove-linked-list-elements.py
@@ -5,21 +5,34 @@
 #         self.next = next
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        if head == None or (head.next == None and head.val != val):
-            return head
-        ans = None
-        previous = None
-        current = head
+        # #Approach1
+        # if head == None or (head.next == None and head.val != val):
+        #     return head
+        # ans = None
+        # previous = None
+        # current = head
 
-        while(current != None):
-            if current.val != val and ans == None:
-                ans = current 
-                previous = current 
-            elif current.val != val:
-                previous.next = current
-                previous = current 
-            current = current.next
-        if previous != None:
-            previous.next = None
-        return ans
-            
+        # while(current != None):
+        #     if current.val != val and ans == None:
+        #         ans = current 
+        #         previous = current 
+        #     elif current.val != val:
+        #         previous.next = current
+        #         previous = current 
+        #     current = current.next
+        # if previous != None:
+        #     previous.next = None
+        # return ans
+
+        #Approach 2
+        holder = ListNode()
+        ans = holder
+        runner = head
+
+        while(runner != None):
+            if runner.val != val:
+                holder.next = runner
+                holder = holder.next
+            runner = runner.next
+        holder.next = None
+        return ans.next
