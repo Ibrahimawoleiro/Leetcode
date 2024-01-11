88327e3708c0046cb2c1ec02c8bd69293c65c60c
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-01-11 17:41:26 -0500

    Time: 266 ms (86.44%) | Memory: 44.1 MB (78.83%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -6,45 +6,23 @@
 #         self.right = right
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-    #     #Approach1-DFS/recursion
-    #     if root == None:
-    #         return 0
-    #     return self.helper(root)
-
-    # def helper(self, current):
-    #     if current.left == None and current.right == None:
-    #         return 1
-    #     if current.left:
-    #         l = 1 + self.helper(current.left)
-    #     if current.right:
-    #         r = 1 + self.helper(current.right)
-    #     if current.left and current.right:
-    #         return min(l,r)
-    #     elif current.left:
-    #         return l
-    #     else:
-    #         return r 
-
-        #Approach2-BFS
-        if root == None:
-            return 0
-        count = 0
-        queue = deque()
-        queue.append(root)
-        queue.append(TreeNode(None,None,None))
-        while(queue):
-            current = queue.popleft()
-            if current.left:
-                queue.append(current.left)
-            if current.right:
-                queue.append(current.right)
-            if current.left == None and current.right == None and current.val!= None:
-                return count + 1
-            if current.val == None:
-                count+=1
-                if(len(queue) > 0):
-                    queue.append(current)
         
-        return count
+        def helper(x):
+            if not x:
+                return 0
+            if not x.left and not x.right:
+                return 1
+
+            if x.left and x.right:
+                return 1 + min(helper(x.left), helper(x.right))
+            
+            if x.right:
+                return 1 + helper(x.right)
+            
+            if x.left:
+                return 1 + helper(x.left)
+
+        return helper(root)
+            
 
         
