c7d6ce89a059549091a283473f11a7a996f09b65
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-01-11 18:05:31 -0500

    Time: 228 ms (96.46%) | Memory: 44.1 MB (85.56%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -6,23 +6,50 @@
 #         self.right = right
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        
-        def helper(x):
-            if not x:
-                return 0
-            if not x.left and not x.right:
-                return 1
+        # #Approach 1
+        # def helper(x):
+        #     if not x:
+        #         return 0
+        #     if not x.left and not x.right:
+        #         return 1
 
-            if x.left and x.right:
-                return 1 + min(helper(x.left), helper(x.right))
+        #     if x.left and x.right:
+        #         return 1 + min(helper(x.left), helper(x.right))
             
-            if x.right:
-                return 1 + helper(x.right)
+        #     if x.right:
+        #         return 1 + helper(x.right)
             
-            if x.left:
-                return 1 + helper(x.left)
+        #     if x.left:
+        #         return 1 + helper(x.left)
 
-        return helper(root)
+        # return helper(root)
+
+        #Approach 2
+        if not root:
+            return 0
+        queue = deque()
+        level = 0
+
+        queue.append(root)
+        queue.append(None)
+
+        while(queue):
+            current = queue.popleft()
+            if current:
+                if current.left:
+                    queue.append(current.left)
+                if current.right:
+                    queue.append(current.right)
+                if not current.left and not current.right:
+                    return level + 1
+            else:
+                level+=1
+                if queue:
+                    queue.append(None)
+
+        return level
+
+
             
 
         
