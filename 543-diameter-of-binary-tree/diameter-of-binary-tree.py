49ea683609b14cf394d7a53ad91497dcf225f5c4
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-01-12 17:02:24 -0500

    Time: 49 ms (49.86%) | Memory: 18.7 MB (40.32%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -6,50 +6,20 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        return self.helper(root)[1]
-        
-    def helper(self,root):
-        if not root.left and not root.right:
-            return [0,0]
-        
-        l = None
-        r = None
 
-        if root.left:
-            l = self.helper(root.left)
-        
-        if root.right:
-            r = self.helper(root.right)
+        def helper(x):
+            if not x:
+                return (0,0)
 
-        if l and r:
-            l[0] = l[0] + 1
-            r[0] = r[0] + 1
-
-            curr = []
-            if l[0] > r[0]:
-                curr.append(l[0])
-            else:
-                curr.append(r[0])
-
-            if l[1] > l[0] + r[0]:
-                curr.append(l[1])
-            elif r[1] > l[0] + r[0]:
-                curr.append(r[1])
-            else:
-                curr.append(l[0] + r[0])
+            l = (0,0)
+            r = (0,0)
+            if x.left:
+                l = helper(x.left)
+            if x.right:
+                r = helper(x.right)
+            print(l[0]+r[0], x.val)
+            return (1 + max(l[0],r[0]), l[0]+r[0] if (l[0]+r[0] > r[1] and l[0]+r[0] > l[1]) else max(r[1], l[1]))
             
-            return curr
-        elif root.right:
-            r[0]+=1
-            if r[0] > r[1]:
-                r[1] = r[0]
-            return r
-        else:
-            l[0]+=1
-            if l[0] > l[1]:
-                l[1] = l[0]
-            return l
-        
-        
-        
-        
+        return helper(root)[1]
+            
+
