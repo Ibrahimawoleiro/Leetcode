95c3ed2fcd4f63d04b7dd118dea5e32a11c916a2
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-01-12 19:47:47 -0500

    Time: 66 ms (79.21%) | Memory: 17.8 MB (53.29%) - LeetSync

--- 617-merge-two-binary-trees/merge-two-binary-trees.py
+++ 617-merge-two-binary-trees/merge-two-binary-trees.py
@@ -6,23 +6,24 @@
 #         self.right = right
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        def helper(x,y):
+            if not x and not y:
+                return None
+    
+            if x and y:
+                curr = TreeNode(x.val + y.val)
+            elif x:
+                y = TreeNode()
+                curr = TreeNode(x.val + y.val)
+            elif y:
+                x = TreeNode()
+                curr = TreeNode(x.val + y.val)
 
-        return self.helper(root1,root2)
+            if x.left or y.left:
+                curr.left = helper(x.left if x.left else TreeNode(), y.left if y.left else TreeNode())
+            
+            if x.right or y.right:
+                curr.right = helper(x.right if x.right else TreeNode(), y.right if y.right else TreeNode())
+            return curr
 
-    def helper(self, r1, r2):
-        if not r1 and not r2:
-            return 
-
-        root = TreeNode(0, None, None)
-        if r1 and not r2:
-            root.val = r1.val
-        if not r1 and r2:
-            root.val = r2.val
-
-        if r1 and r2:
-            root.val = r1.val + r2.val
-
-        root.left = self.helper(r1.left if r1 else None, r2.left if r2 else None)
-        root.right = self.helper(r1.right if r1 else None, r2.right if r2 else None)
-
-        return root
+        return helper(root1,root2)
