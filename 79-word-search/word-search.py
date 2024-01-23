c5f532bddba37d7b3adf8f1509c11253d2dddbf6
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-01-22 22:36:43 -0500

    Time: 3680 ms (79.44%) | Memory: 16.6 MB (59.59%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,38 +1,35 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         
-        for row_index in range(len(board)):
-            for col_index in range(len(board[0])):
-                if board[row_index][col_index] == word[0]:
-                    seen = set()
-                    i = 1
-                    if self.helper([row_index,col_index, i], seen,board,word):
+        def helper(save, curr,index):
+            if curr not in seen and board[curr[0]][curr[1]] == word[index]:
+                if index == len(word) - 1:
+                    return True
+                save.add(curr)
+                if curr[0] > 0:
+                    if helper(save,(curr[0] - 1,curr[1]),index + 1):
                         return True
+                if curr[0] < len(board) - 1:
+                    if helper(save,(curr[0] + 1,curr[1]),index + 1):
+                        return True
+                if curr[1] > 0:
+                    if helper(save,(curr[0],curr[1] - 1),index + 1):
+                        return True
+                if curr[1] < len(board[0]) - 1:
+                    if helper(save,(curr[0],curr[1] + 1),index + 1):
+                        return True
+                save.remove(curr)
+            else:
+                return False
+
+        seen = set()
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+
+                if word[0] == board[row][col]:
+                    if helper(seen,(row,col),0):
+                        return True
+
         return False
 
-    def helper(self, curr, seen,board,word):
-        if curr[2] == len(word):
-            return True
-        seen.add((curr[0] , curr[1]))
-        if(curr[0] - 1 >= 0 and (curr[0] - 1  , curr[1]) not in seen):
-            if board[curr[0] - 1][curr[1]] == word[curr[2]]:
-                if self.helper([curr[0] - 1  , curr[1], curr[2] + 1],seen,board,word):
-                    return True
-
-        if(curr[0] + 1 < len(board) and (curr[0] + 1  , curr[1]) not in seen):
-            if board[curr[0] + 1][curr[1]] == word[curr[2]]:
-                if self.helper([curr[0] + 1  , curr[1] , curr[2] + 1],seen,board,word):
-                    return True
-                
-
-        if(curr[1] + 1 < len(board[0]) and (curr[0] , curr[1] + 1) not in seen):
-            if board[curr[0]][curr[1] + 1] == word[curr[2]]:
-                if self.helper([curr[0] , curr[1] + 1, curr[2] + 1],seen,board,word):
-                    return True
-
-        if(curr[1] - 1 >= 0 and (curr[0] , curr[1] - 1) not in seen):
-            if board[curr[0]][curr[1] - 1] == word[curr[2]]:
-                if self.helper([curr[0] , curr[1] - 1, curr[2] + 1],seen,board,word):
-                    return True
-
-        seen.remove((curr[0] , curr[1]))
+        
