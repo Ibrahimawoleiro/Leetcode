1475555e54f026931ced5524f95ea4ed4855e936
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-01-23 20:38:54 -0500

    Time: 165 ms (22.20%) | Memory: 17 MB (59.64%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,32 +1,17 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        #Approach1
+        #Approach 1
+        array = set()
 
-        result = []
-        marker = []
-        self.helper(marker, result, nums)
-        return result
-    
-    def helper(self,marker, result, curr_arr,seen=None):
-        if seen == None:
-            seen = set()
+        def helper(arr,permutation):
+            if len(permutation) == len(nums):
+                array.add(tuple(permutation.copy()))
+                return
+            for index in range(len(arr)):
+                permutation.append(arr.pop(0)) 
+                helper(arr,permutation)
+                arr.append(permutation.pop())
+        helper(nums.copy(),[])
 
-        if not curr_arr:
-            result.append(marker.copy())
-            return
-
-        for val in range(len(curr_arr)):
-            current = curr_arr[0]
-            if current not in seen:
-                seen.add(current)
-            else:
-                temp = curr_arr.pop(0)
-                curr_arr.append(temp)
-                continue
-            marker.append(current)
-            print(marker)
-            copy = curr_arr[1:len(curr_arr)]
-            self.helper(marker, result, copy)
-            marker.pop()
-            temp = curr_arr.pop(0)
-            curr_arr.append(temp)
+        return list(array)
+            
