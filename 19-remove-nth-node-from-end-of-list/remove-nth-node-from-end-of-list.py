b69dfb04b1c1451caba03fc0732efd982c82982a
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-01-30 22:32:16 -0500

    Time: 35 ms (79.80%) | Memory: 16.5 MB (62.92%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -5,23 +5,43 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        #Approach 1
+        # if head.next == None and n>0:
+        #     return None
+        # result = ListNode()
+        # result.next = head
+        # fast = result
+        # slow = result
 
-        if head.next == None and n>0:
-            return None
-        result = ListNode()
-        result.next = head
-        fast = result
-        slow = result
+        # while fast != None:
+        #     if n+1 > 0:
+        #         fast = fast.next
+        #         n-=1
+        #     else:
+        #         fast = fast.next
+        #         slow = slow.next
 
-        while fast != None:
-            if n+1 > 0:
-                fast = fast.next
-                n-=1
-            else:
-                fast = fast.next
-                slow = slow.next
-
-        if slow.next!=None:
-            slow.next = slow.next.next
+        # if slow.next!=None:
+        #     slow.next = slow.next.next
         
-        return result.next
+        # return result.next
+
+        #Approach 2
+        delayed = ListNode()
+        ans = delayed
+        delayed.next = head
+        runner = head
+
+        while(runner):
+            if n > 0:
+                runner = runner.next
+                n-=1
+                continue
+            runner = runner.next
+            delayed = delayed.next
+
+        delayed.next = delayed.next.next
+
+        return ans.next
+            
+
