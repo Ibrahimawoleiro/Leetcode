86ad6a7c069510f7c23e96e35b1271e7b234a45e
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-04-02 13:31:41 -0400

    Time: 53 ms (86.39%) | Memory: 51.1 MB (70.71%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,19 +3,37 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let dictionary = {}
+    // //1
+    // let dictionary = {}
 
-    nums.forEach((curr)=>{
-        if(curr in dictionary){
-            dictionary[curr]+=1
-        }else{
-            dictionary[curr] = 1
+    // nums.forEach((curr)=>{
+    //     if(curr in dictionary){
+    //         dictionary[curr]+=1
+    //     }else{
+    //         dictionary[curr] = 1
+    //     }
+    // })
+
+    // for(let key in dictionary){
+    //     if(dictionary[key] > Math.floor(nums.length/2)){
+    //         return key
+    //     }
+    // }
+
+    //2
+    let count = 0
+    let curr = 0
+    for(let i = 0; i < nums.length;i++){
+        if (count == 0){
+            count = 1
+            curr = nums[i]
+            continue
         }
-    })
-
-    for(let key in dictionary){
-        if(dictionary[key] > Math.floor(nums.length/2)){
-            return key
+        if (nums[i] == curr){
+            count +=1
+        }else{
+            count -=1
         }
     }
+    return curr
 };
