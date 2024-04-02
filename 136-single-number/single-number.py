a7d8c4ca13c08a1431e37e0facff909dcd61b054
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-04-02 12:17:01 -0400

    Time: 111 ms (57.88%) | Memory: 19.6 MB (7.63%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,36 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        # #Approach 1
-        # checker = set()
-        # for val in nums:
-        #     if val not in checker:
-        #         checker.add(val)
-        #     else:
-        #         checker.remove(val)
-        
-        # return checker.pop()
-
-        # #Approach 2
-        
-        # for index in range(len(nums)):
-        #     count = 0
-        #     for checker in range(len(nums)):
-        #         if index == checker:
-        #             continue
-                
-        #         if nums[checker] == nums[index]:
-        #             count+=1
-
-        #         if count > 0:
-        #             break
-        #     if count == 0:
-        #         return nums[index]
-
-        # #Approach 3
-
-        res = 0
-        for n in nums:
-            res = n ^ res
-        return res
-
-
+        #1
+        s = set()
+        for val in nums:
+            if val not in s:
+                s.add(val)
+            else:
+                s.remove(val)
+        return s.pop()
