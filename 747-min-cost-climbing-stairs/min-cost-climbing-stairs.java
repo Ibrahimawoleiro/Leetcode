bb0ddf8d1f3980a48aadab3a4dc10a2bd6608175
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-04-03 02:16:45 -0400

    Time: 6 ms (8.49%) | Memory: 43.1 MB (47.38%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -14,25 +14,35 @@ class Solution {
     // }
 
 
-        //2
-        HashMap<Integer,Integer> store = new HashMap<>();
-        return Math.min( Math.min(cost[0]+r(1,cost, store),cost[0]+r(2,cost, store)), Math.min(cost[1]+r(2,cost,store),cost[1]+r(3,cost,store)));
-    }
+    //     //2
+    //     HashMap<Integer,Integer> store = new HashMap<>();
+    //     return Math.min( Math.min(cost[0]+r(1,cost, store),cost[0]+r(2,cost, store)), Math.min(cost[1]+r(2,cost,store),cost[1]+r(3,cost,store)));
+    // }
 
-    public int r(int index, int[] cost, HashMap<Integer,Integer> store){
-            if(index >= cost.length){
-                return 0;
-            }else if(index == cost.length - 1){
-                return cost[index];
+    // public int r(int index, int[] cost, HashMap<Integer,Integer> store){
+    //         if(index >= cost.length){
+    //             return 0;
+    //         }else if(index == cost.length - 1){
+    //             return cost[index];
+    //         }
+    //         if(store.containsKey(index)){
+    //             return store.get(index);
+    //         }
+    //         int curr = Math.min(cost[index] + r(index + 1, cost, store), cost[index] + r(index + 2, cost, store));
+    //         store.put(index, curr);
+    //         return curr;
+    // }
+
+        int i = cost.length - 1;
+        while(i >= 0){
+            if(i == cost.length - 1 || i+1 >= cost.length || i + 2 >= cost.length){
+                i--;
+                continue;
             }
-            if(store.containsKey(index)){
-                return store.get(index);
-            }
-            int curr = Math.min(cost[index] + r(index + 1, cost, store), cost[index] + r(index + 2, cost, store));
-            store.put(index, curr);
-            return curr;
+            cost[i] = Math.min(cost[i] + cost[i + 1], cost[i]+ cost[i+2]);
+            i--;
+        }
+        System.out.print(Arrays.toString(cost));
+        return Math.min(cost[0], cost[1]);
     }
-
-
-    
 }
