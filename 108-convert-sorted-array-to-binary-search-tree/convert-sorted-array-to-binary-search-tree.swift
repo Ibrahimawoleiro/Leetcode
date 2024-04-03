c986a72a031eab092e76c6569e619d8c46a11af5
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-04-02 22:54:24 -0400

    Time: 19 ms (11.79%) | Memory: 16.3 MB (76.42%) - LeetSync

---
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.swift
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func sortedArrayToBST(_ nums: [Int]) -> TreeNode? {
+        var high: Int = nums.count - 1
+        var low: Int = 0
+        return recursive(low, high, array: nums)
+    }
+
+    func recursive(_ low:Int, _ high: Int, array nums:[Int]) -> TreeNode?{
+        if low > high{
+            return nil
+        }
+        if low == high{
+            return TreeNode(nums[high])
+        }
+        
+        var mid: Int = Int((high+low)/2)
+        print("low \(low) mid \(mid) high \(high)")
+        print(nums[mid])
+        var curr = TreeNode(nums[mid])
+
+        curr.left = recursive(low, mid - 1, array: nums)
+
+        curr.right = recursive(mid + 1, high, array: nums)
+
+        return curr
+    }
+}
