98942801b21b4b552fb0eef3d796a5f205f2d17c
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-04-03 16:41:21 -0400

    Time: 10 ms (11.87%) | Memory: 45.3 MB (8.83%) - LeetSync

---
+++ 733-flood-fill/flood-fill.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int f = image[sr][sc];
+        recursive(sr,sc,f,color,image);
+        return image;
+    }
+
+    void recursive(int r, int c,int f, int i, int[][] arr){
+        if(r<0 || r>= arr.length || c<0 || c>= arr[0].length || arr[r][c] != f || arr[r][c] == i){
+            return ;
+        }
+
+        arr[r][c] = i;
+        System.out.println("row"+r+"column"+c);
+        recursive(r-1,c,f,i,arr);
+        recursive(r+1,c,f,i,arr);
+        recursive(r,c-1,f,i,arr);
+        recursive(r,c+1,f,i,arr);
+    }
+}
