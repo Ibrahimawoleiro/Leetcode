3d8f5b51f56e81491723fabe4c9e6a1995336c99
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-04-03 16:14:28 -0400

    Time: 163 ms (5.08%) | Memory: 59.9 MB (98.12%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.ts
+++ 217-contains-duplicate/contains-duplicate.ts
@@ -1,12 +1,30 @@
 function containsDuplicate(nums: number[]): boolean {
-    let store: Set<number> = new Set()
-    let i:number = 0
-    while(i < nums.length){
-        if(store.has(nums[i])){
+    // //1
+    // let store: Set<number> = new Set()
+    // let i:number = 0
+    // while(i < nums.length){
+    //     if(store.has(nums[i])){
+    //         return true
+    //     }
+    //     store.add(nums[i])
+    //     i+=1
+    // }
+    // return false
+
+    if (nums.length == 1){
+        return false
+    }
+
+    nums.sort()
+
+    let i: number | String = 0
+    let j: number | boolean = 1
+    while(j< nums.length){
+        if (nums[i] === nums[j]){
             return true
         }
-        store.add(nums[i])
         i+=1
+        j+=1
     }
     return false
 };
