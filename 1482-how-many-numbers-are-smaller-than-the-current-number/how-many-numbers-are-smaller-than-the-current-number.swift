0d05134cf3e021facf3da481a1209051b63f8693
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-04-04 19:46:37 -0400

    Time: 24 ms (7.38%) | Memory: 16.1 MB (36.91%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.swift
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.swift
@@ -1,11 +1,32 @@
 class Solution {
     func smallerNumbersThanCurrent(_ nums: [Int]) -> [Int] {
+        // //1
+        // var count: Int = 0
+        // var result: [Int] = Array(repeating: 0, count: nums.count)
+        // for index in 0..<nums.count{
+        //     for checker_index in 0..<nums.count{
+        //         if index == checker_index{
+        //             continue
+        //         }
+        //         if nums[checker_index] < nums[index]{
+        //             count+=1
+        //         }
+        //     }
+        //     result[index] = count
+        //     count = 0
+        // }
+        // return result
+
+        //2
         var count: Int = 0
+        var store: [Int: Int] = [:]
         var result: [Int] = Array(repeating: 0, count: nums.count)
         for index in 0..<nums.count{
             for checker_index in 0..<nums.count{
                 if index == checker_index{
                     continue
+                }else if store.keys.contains(nums[index]){
+                    result[index] = store[nums[index]]!
                 }
                 if nums[checker_index] < nums[index]{
                     count+=1
