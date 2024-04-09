c7eb776f341e473366fef2fe48e02aab8081ad82
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-04-09 11:04:40 -0400

    Time: 70 ms (59.18%) | Memory: 54.7 MB (64.63%) - LeetSync

--- 965-unique-email-addresses/unique-email-addresses.ts
+++ 965-unique-email-addresses/unique-email-addresses.ts
@@ -3,11 +3,8 @@ function numUniqueEmails(emails: string[]): number {
     for(let email of emails){
         let splitted: string[] = email.split('@')
         let removed_plus: string = splitted[0].split('+')[0]
-
         removed_plus = removed_plus.replace(/\./g,'')
-        console.log(removed_plus)
         store.add(removed_plus+'@'+splitted[1])
     }
-    console.log(store)
     return store.size
 };
