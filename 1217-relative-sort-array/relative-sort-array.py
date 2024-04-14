0e25628df907bf4a5f77552cca3e22e93cbaec74
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-04-14 01:31:11 -0400

    Time: 43 ms (42.90%) | Memory: 17 MB (12.53%) - LeetSync

---
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -0,0 +1,33 @@
+import queue
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        checker = set()
+        for val in arr2:
+            checker.add(val)
+        q = []
+        store = {}
+        for val in arr1:
+            if val not in checker:
+                q.append(val)
+                checker.add(val)
+            if val not in store:
+                store[val] = 1
+            else:
+                store[val] += 1
+        
+        ans = []
+        for val in arr2:
+            while(store[val] > 0):
+                ans.append(val)
+                store[val] -= 1
+        q.sort()
+        q.reverse()
+        while(q):
+            curr = q.pop()
+            print(curr)
+            while(store[curr] > 0):
+                ans.append(curr)
+                store[curr] -= 1
+
+        return ans
+
