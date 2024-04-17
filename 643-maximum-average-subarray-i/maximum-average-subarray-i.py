7d0ea0f1f97bc7691bbb1e839222a29b244c87c4
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-04-17 00:44:33 -0400

    Time: 885 ms (66.92%) | Memory: 28.7 MB (16.03%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -1,42 +1,22 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        # #Approach1
-        # nums_pointer = 0
-        # max_average = -10 ** 6
-        # if len(nums) == 1:
-        #     return nums[0]
-        # while(nums_pointer + k-1 < len(nums)):
-        #     i = nums_pointer
-        #     sum = 0
-        #     while(i < nums_pointer + k):
-  
-        #         sum += nums[i]
-        #         i+=1
-        #     if sum/k > max_average:
-        #         max_average = sum/k
-        #     nums_pointer+=1
+        i = 0
+        j = k - 1
+        sum_subarr = sum(nums[0:k])
+        max_subarr = sum_subarr / k
+        while j < len(nums):
+            if j == k - 1:
+                j+=1
+                sum_subarr -= nums[i]
+                i+=1
+                continue
+            sum_subarr += nums[j]
+            curr_subarr_avg = sum_subarr / k
+            if curr_subarr_avg > max_subarr:
+                max_subarr = curr_subarr_avg
+            sum_subarr -= nums[i]
+            i+=1
+            j+=1
+            
         
-        # return max_average
-
-        #Appraoch2
-        nums_pointer = 0
-        max_average = -10 ** 6
-        sum = 0
-        end = nums_pointer + k - 1
-        while(end < len(nums)):
-            if nums_pointer == 0:
-                i = 0
-                while(i <= end):
-                    sum+=nums[i]
-                    i+=1 
-                max_average = sum/k
-            else:
-                sum -= nums[nums_pointer - 1]
-                sum += nums[end]
-                if sum / k > max_average:
-                    max_average = sum / k
-
-            nums_pointer += 1
-            end += 1
-
-        return max_average   
+        return max_subarr
