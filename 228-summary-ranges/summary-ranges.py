4d0654b9054774695ddff5cda1c4279514d43b4f
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-04-19 02:12:52 -0400

    Time: 27 ms (93.02%) | Memory: 16.5 MB (45.98%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.py
@@ -0,0 +1,32 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        curr_range = ''
+        ans = []
+        i = 0
+        j = 0
+        while(j < len(nums)):
+            print(i,'i',j,'j')
+            if i == j:
+                curr_range += str(nums[i])
+                j+=1
+                continue
+            elif nums[j] - nums[i] > 1:
+                print(curr_range, nums[i],i,j)
+                if curr_range != str(nums[i]):
+                    curr_range+= f'->{str(nums[i])}'
+                ans.append(curr_range)
+                curr_range = ''
+                i+=1
+                continue
+            i+=1
+            j+=1
+
+        if len(curr_range) > 0:
+            print(curr_range,"hbjsdkx", str(nums[-1]))
+            if str(nums[-1]) != (curr_range):
+                curr_range+=f'->{nums[-1]}'
+                ans.append(curr_range)
+            else:
+                ans.append(curr_range)
+        
+        return ans
