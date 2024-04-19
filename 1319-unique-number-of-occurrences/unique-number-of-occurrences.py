145d3265ee1c4916f579f33345855f6e6c547442
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-04-19 17:51:49 -0400

    Time: 39 ms (73.05%) | Memory: 16.7 MB (29.94%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -2,7 +2,7 @@ class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         store = collections.Counter(arr)
         checker = set()
-        for key, value in store.items():
+        for value in store.values():
             if value in checker:
                 return False
             checker.add(value)
