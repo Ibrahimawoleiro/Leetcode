9ca29bc5240020dfdaac2df25f7e44817a3aaf13
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-04-20 02:10:35 -0400

    Time: 39 ms (42.78%) | Memory: 16.6 MB (80.77%) - LeetSync

--- 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.py
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.py
@@ -1,9 +1,6 @@
 class Solution:
     def sumZero(self, n: int) -> List[int]:
         ans = [k+1 for k in range(n)]
-
         balancer = ((n-1)*(n))//2
-
         ans[-1] = -1 * balancer
-
         return ans
