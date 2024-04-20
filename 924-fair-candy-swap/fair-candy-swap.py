e424078f4ff975a5278b4ac5417e941b91b11b80
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-04-19 21:17:57 -0400

    Time: 338 ms (31.29%) | Memory: 19 MB (74.05%) - LeetSync

---
+++ 924-fair-candy-swap/fair-candy-swap.py
@@ -0,0 +1,22 @@
+class Solution:
+    def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
+        alice_sum = sum(aliceSizes)
+        bob_sum = sum(bobSizes)
+
+        aliceSizes.sort()
+        bobSizes.sort()
+
+        i = len(aliceSizes) - 1
+        j = len(bobSizes) - 1
+
+        while(i >= 0 and j >= 0):
+            print(alice_sum - aliceSizes[i] ,bob_sum - bobSizes[j])
+            if alice_sum - aliceSizes[i] + bobSizes[j] == bob_sum - bobSizes[j] + aliceSizes[i]:
+                return [aliceSizes[i], bobSizes[j]]
+            elif alice_sum - aliceSizes[i] + bobSizes[j] > bob_sum - bobSizes[j] + aliceSizes[i]:
+                j-=1
+            else:
+                i-=1
+        
+        return []
+
