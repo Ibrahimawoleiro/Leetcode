092dd59faf74b81f006f3417767d40e80e5fbf29
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-04-21 13:35:15 -0400

    Time: 18 ms (7.59%) | Memory: 42.1 MB (5.37%) - LeetSync

---
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int one_students = 0;
+        int zero_students = 0;
+        //Stack<Integer> stack = new Stack<>();
+        int i = 0;
+        for(int n: students){
+            if(n == 1){
+                one_students+=1;
+            }else{
+                zero_students+=1;
+            }
+        }
+        while(i < sandwiches.length){
+            int curr = sandwiches[i];
+            System.out.println("Top of stack "+curr+" one_students"+one_students+" 0"+zero_students);
+            if((curr == 0 && zero_students == 0) || (curr == 1 && one_students == 0)){
+                return one_students + zero_students;
+            }else if(curr == 0){
+                i+=1;
+                zero_students -= 1;
+            }else{
+                i+=1;
+                one_students -= 1;
+            }
+        }
+
+        return 0;
+    }
+}
