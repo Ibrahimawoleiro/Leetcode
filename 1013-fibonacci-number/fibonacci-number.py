0ff2bab944157b6ea1fd6443823b623c36d18162
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-04-20 21:15:02 -0400

    Time: 346 ms (24.33%) | Memory: 16.6 MB (25.88%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,35 +1,8 @@
 class Solution:
-
     def fib(self, n: int) -> int:
-
-        if(n==0):
+        if n == 0:
             return 0
-        
-        if(n==1):
+        if n == 1:
             return 1
         
-        
-        dictionary = {
-            0:0,
-            1:1
-        }
-
-        left = self.helper(dictionary,n-1)
-
-        right = self.helper(dictionary,n-2)
-
-        return left + right
-
-    def helper(self,dictionary, n):
-        if n in dictionary:
-            return dictionary[n]
-
-        left = self.helper(dictionary,n-1)
-
-        right = self.helper(dictionary,n-2)
-
-        dictionary[n]=left+right
-
-        return left + right
-
-        
+        return self.fib(n - 1) + self.fib(n - 2)
