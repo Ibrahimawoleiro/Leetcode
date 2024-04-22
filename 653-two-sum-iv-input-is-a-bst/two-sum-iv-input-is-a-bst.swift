446be9bf59d98fef66de47c518a03172fefc0f47
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-04-22 19:01:19 -0400

    Time: 55 ms (57.69%) | Memory: 17.2 MB (26.92%) - LeetSync

---
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.swift
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func findTarget(_ root: TreeNode?, _ k: Int) -> Bool {
+        var store: Set<Int> = Set<Int>();
+        var count: Int = 2;
+        var ans: Bool? = self.recursive(root, &store, k);
+        if let ans{
+            return true;
+        }
+        return false;
+    }
+    func recursive(_ root: TreeNode?, _ store: inout Set<Int>, _ k: Int) -> Bool?{
+        guard let root = root else{return nil};
+
+        if store.contains(k - root.val ){
+            return true;
+        }
+
+        store.insert(root.val);
+
+        if let l = self.recursive(root.left, &store, k){
+            return true;
+        }else if let r = self.recursive(root.right, &store, k){
+            return true;
+        }
+        return nil;
+    }
+}
