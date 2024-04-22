ed5700fe993aea31a379794dd30a58602ba6874b
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-04-22 14:09:11 -0400

    Time: 2 ms (95.91%) | Memory: 15.5 MB (28.93%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.swift
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func isSymmetric(_ root: TreeNode?) -> Bool {
+        if root!.right == nil && root!.left == nil{
+            return true;
+        }else if root!.right == nil || root!.left == nil{
+            return false;
+        }
+        return self.recursive(left_child: root!.left, right_child: root!.right);
+    }
+
+    func recursive(left_child left: TreeNode?, right_child right: TreeNode?) -> Bool{
+        if(left == nil && right == nil){
+            return true;
+        }else if(left == nil || right == nil){
+            return false;
+        }
+        if let left = left, let right = right{
+            if left.val == right.val{
+                return self.recursive(left_child : left.left, right_child : right.right) && self.recursive(left_child : left.right, right_child : right.left)
+            }else{
+                return false;
+            }
+        }
+        return false;
+    }
+}
