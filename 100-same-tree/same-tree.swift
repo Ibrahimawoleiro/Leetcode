a6580e1b100ebbcd5403b967da64c32a611a8be6
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-04-22 18:12:54 -0400

    Time: 2 ms (74.27%) | Memory: 15.5 MB (7.52%) - LeetSync

---
+++ 100-same-tree/same-tree.swift
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        return self.recursive(p, q);
+    }
+
+    func recursive(_ left: TreeNode?, _ right: TreeNode?) -> Bool{
+        if let left = left , let right = right{
+
+            if left.val != right.val{
+                print("left \(left.val) right \(right.val)");
+                return false
+            }
+            return self.recursive(left.left, right.left) && self.recursive(left.right, right.right);
+        }else if let left = left{
+            return false;
+        }else if let right = right{
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
