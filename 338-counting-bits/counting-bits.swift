3efdd1d68be82cc4f91762b3c21949ea8293ac4c
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-04-24 13:02:18 -0400

    Time: 90 ms (6.67%) | Memory: 21.4 MB (6.15%) - LeetSync

---
+++ 338-counting-bits/counting-bits.swift
@@ -0,0 +1,23 @@
+class Solution {
+    func countBits(_ n: Int) -> [Int] {
+        var store : [Int] = Array(repeating:0, count:n+1);
+        var r: Int = 0;
+        var helper: Int = 0;
+        var x: Int = 2;
+        while(r <= n){
+            print(r);
+            if r == 0{
+                r+=1;
+                helper += 1;
+                continue;
+            }
+            if r == x{
+                helper = x;
+                x = x * 2;
+            }
+            store[r] = 1 + store[r - helper];
+            r+=1;
+        }
+        return store;
+    }
+}
