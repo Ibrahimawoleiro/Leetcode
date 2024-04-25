477090a05357f1435b835efbf4f814dbee0193cc
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-04-25 17:21:54 -0400

    Time: 156 ms (5.41%) | Memory: 59.4 MB (5.41%) - LeetSync

---
+++ 415-add-strings/add-strings.ts
@@ -0,0 +1,52 @@
+function addStrings(num1: string, num2: string): string {
+    let num1_last : number = num1.length - 1;
+    let num2_last : number = num2.length - 1;
+    let ans: string[] = [];
+    let remainder: number = 0;
+    while(num1_last >= 0 || num2_last >= 0){
+        if (num1_last >= 0 && num2_last >= 0){
+            const s1: string = num1.charAt(num1_last);
+            const s2: string = num2.charAt(num2_last);
+            let curr: number = parseInt(s1) + parseInt(s2) + remainder;
+            if(curr >= 10){
+                remainder = 1;
+                curr = curr % 10;
+            }else{
+                remainder = 0;
+            }
+            ans.push(`${curr}`);
+            console.log(`s1 ${s1} s2 ${s2} curr ${curr} remainder ${remainder}`);
+            num1_last--;
+            num2_last--;
+        }else if (num1_last >= 0){
+            const s1: string = num1.charAt(num1_last);
+            let curr: number = parseInt(s1) + remainder;
+             if(curr >= 10){
+                remainder = 1;
+                curr = curr % 10;
+            }else{
+                remainder = 0;
+            }
+            ans.push(`${curr}`);
+            console.log(`s1 ${s1} curr ${curr} remainder ${remainder}`);
+            num1_last--;
+        }else{
+            const s2: string = num2.charAt(num2_last);
+            let curr: number = parseInt(s2) + remainder;
+             if(curr >= 10){
+                remainder = 1;
+                curr = curr % 10;
+            }else{
+                remainder = 0;
+            }
+            ans.push(`${curr}`);
+            console.log(`s2 ${s2} curr ${curr} remainder ${remainder}`);
+            num2_last--;
+        }
+    }
+    if(remainder > 0){
+        ans.push(`${remainder}`)
+    }
+    let result : string[] = ans.reverse();
+    return result.join("")
+};
