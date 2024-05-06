f50b2a91601bec8da7d51010ea555ccab70667b4
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-05-05 20:46:04 -0400

    Time: 303 ms (5.05%) | Memory: 26.2 MB (12.89%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,37 +1,19 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # #Appraoch1->Time limit Exceeded
-        # result = [0 for val in range(len(nums))]
-
-        # for index in range(len(nums)):
-        #     curr = 1
-        #     for val in range(len(nums)):
-        #         if val == index:
-        #             continue
-        #         curr *= nums[val]
-
-        #     result[index] = curr
-            
-        # return result
-
-        #Approach2
         prefix = [1 for val in range(len(nums))]
         postfix = [1 for val in range(len(nums))]
-
+        ans = [1 for val in range(len(nums))]
         for index in range(len(nums)):
             if index == 0:
                 continue
             prefix[index] = prefix[index - 1] * nums[index - 1]
 
-        for index in reversed(range(len(nums))):
+        for index in range(len(nums) - 1, -1, -1):
             if index == len(nums) - 1:
                 continue
             postfix[index] = postfix[index + 1] * nums[index + 1]
 
-        for val in range(len(postfix)):
-            postfix[val] = postfix[val] * prefix[val]
-
-        return postfix
-
-
-
+        for index in range(len(nums)):
+            ans[index] = postfix[index] * prefix[index]
+        
+        return ans
