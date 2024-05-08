29f63a37bca49a7d39145b3c063ebbf02759f998
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-05-07 21:50:06 -0400

    Time: 4459 ms (31.67%) | Memory: 16.7 MB (25.11%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,35 +1,32 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        
-        def helper(save, curr,index):
-            if curr not in seen and board[curr[0]][curr[1]] == word[index]:
-                if index == len(word) - 1:
+        #curr is an arr of row, col
+        #c is a set for checking
+        c = set()
+        def helper(i, curr):
+            if board[curr[0]][curr[1]] != word[i] or (curr[0],curr[1]) in c:
+                return
+            if i == len(word) - 1:
+                return True
+            c.add((curr[0], curr[1]))
+            
+            if curr[0] > 0:
+                if helper(i+1, [curr[0] - 1, curr[1]]):
                     return True
-                save.add(curr)
-                if curr[0] > 0:
-                    if helper(save,(curr[0] - 1,curr[1]),index + 1):
-                        return True
-                if curr[0] < len(board) - 1:
-                    if helper(save,(curr[0] + 1,curr[1]),index + 1):
-                        return True
-                if curr[1] > 0:
-                    if helper(save,(curr[0],curr[1] - 1),index + 1):
-                        return True
-                if curr[1] < len(board[0]) - 1:
-                    if helper(save,(curr[0],curr[1] + 1),index + 1):
-                        return True
-                save.remove(curr)
-            else:
-                return False
-
-        seen = set()
-        for row in range(len(board)):
-            for col in range(len(board[0])):
-
-                if word[0] == board[row][col]:
-                    if helper(seen,(row,col),0):
-                        return True
-
+            if curr[0] < len(board) - 1:
+                if helper(i + 1, [curr[0] + 1, curr[1]]):
+                    return True
+            if curr[1] > 0:
+                if helper(i + 1, [curr[0], curr[1] - 1]):
+                    return True
+            if curr[1] < len(board[0]) - 1:
+                if helper(i + 1, [curr[0], curr[1] + 1]):
+                    return True
+            c.remove((curr[0], curr[1]))
+              
+        for r_index in range(len(board)):
+            for c_index in range(len(board[0])):
+                    if board[r_index][c_index] == word[0]:
+                        if helper(0, [r_index,c_index]):
+                            return True
         return False
-
-        
