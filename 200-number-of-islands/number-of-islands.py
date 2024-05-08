689468cbf8f00f46fa9f919d7100959d348a63af
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-05-07 20:35:36 -0400

    Time: 229 ms (81.21%) | Memory: 18.9 MB (81.46%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,49 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        #Create a counter
+        #Loop through the 2D array 
+        #When you find an island, increase counter
+            #Find all the land connected to current island and mark them as seen 
+        #return the counter
+
+        counter = 0
+        for r_index in range(len(grid)):
+            for c_index in range(len(grid[0])):
+                if grid[r_index][c_index] == "1":
+                    counter += 1
+                    stack = [[r_index, c_index]]
+                    while(stack):
+                        curr = stack.pop()
+                        grid[curr[0]][curr[1]] = 5
+                        if curr[0] > 0 and grid[curr[0] - 1] [curr[1]] == '1':
+                            stack.append([curr[0] - 1, curr[1]])
+                        if curr[0] < len(grid) - 1 and grid[curr[0] + 1][curr[1]] == '1':
+                            stack.append([curr[0] + 1, curr[1]])
+                        if curr[1] > 0 and grid[curr[0]][curr[1] - 1] == '1':
+                            stack.append([curr[0],curr[1] - 1])
+                        if curr[1] < len(grid[0]) - 1 and grid[curr[0]][curr[1] + 1] == '1':
+                            stack.append([curr[0],curr[1] + 1])
         
-        island_count = 0
- 
-        Queue = deque()
-        k=0
-        for array_index in range(len(grid)):
-            for number_index in range(len(grid[array_index])):
-                if grid[array_index][number_index] == "1":
-                
-                    island_count +=1
-                    grid[array_index][number_index] = "5"
-                    if array_index-1>=0 and grid[array_index-1][number_index]=="1":
-                        Queue.append([array_index-1,number_index])
-                    if array_index+1<len(grid) and grid[array_index+1][number_index]=="1":
-                        Queue.append([array_index+1,number_index])
-                    if number_index -1 >= 0 and grid[array_index][number_index-1]=="1":
-                        Queue.append([array_index,number_index-1])
-                    if number_index + 1 < len(grid[0]) and grid[array_index][number_index+1]=="1":
-                        Queue.append([array_index,number_index+1])
-             
-                    while Queue:
-                   
-                        curr = Queue.popleft()
-                     
-                        grid[curr[0]][curr[1]]="5"
-                        if curr[0] - 1 >= 0 and grid[curr[0]-1][curr[1]]=="1":
-                            Queue.append([curr[0]-1,curr[1]])
-                            grid[curr[0]-1][curr[1]]="5"
-                
-                        if curr[0] + 1 < len(grid) and grid[curr[0]+1][curr[1]] == "1":
-                            Queue.append([curr[0]+1,curr[1]])
-                            grid[curr[0]+1][curr[1]]="5"
-                            
-                       
-                        if curr[1]-1 >= 0 and grid[curr[0]][curr[1]-1] == "1":
-                            Queue.append([curr[0],curr[1]-1])
-                            grid[curr[0]][curr[1]-1]="5"
-                            
-                        if curr[1] + 1 < len(grid[0]) and grid[curr[0]][curr[1]+1] == "1":
-                            Queue.append([curr[0],curr[1]+1])
-                            grid[curr[0]][curr[1]+1]="5"
-                            
-                        
-        return island_count
-                        
-                
+        return counter
                         
