36150ca4c4c60cffc5d7a36ef2f1031c10174505
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-05-09 11:57:01 -0400

    Time: 46 ms (21.15%) | Memory: 16.9 MB (10.70%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,25 +1,19 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-    #I need an empty array to store the overall output
-    #I need an empty array to store the current permutation i am making
-
-        array = []
-
-        def helper(arr,permutation):
-            if len(permutation) == len(nums):
-                array.append(permutation.copy())
-                return
-            for index in range(len(arr)):
-                permutation.append(arr.pop(0)) 
-                helper(arr,permutation)
-                arr.append(permutation.pop())
-        helper(nums.copy(),[])
-
-        return array
+        ans= []
+        def helper(curr):
+            if len(nums) == 0:
+                ans.append(curr.copy())
+                return 
             
+            for count in range(len(nums)):
+                c = nums.pop(0)
+                curr.append(c)
+                print("k", len(nums), nums, curr)
+                helper(curr)
+                nums.append(c)
+                curr.pop()
+            return ans
 
+        return helper([])
 
-
-
-
-        
