d3cea5af4719d122cb00194558620945d9f28a23
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-05-10 18:44:43 -0400

    Time: 617 ms (5.03%) | Memory: 35.4 MB (16.43%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,38 +1,31 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-
+        #Every partition is a beginning 
+        #And every partition should move from size 1 to the end of the array
+        #Every partition should not move to the next size unless the current size is a palindrome
         ans = []
-        curr = []
-
-        def isPalindrome(start, end):
-            while(start <= end):
-                if s[start] != s[end]:
-                    return False
-                start += 1
-                end -= 1
-            return True
-
-        def helper(current, start):
-            if start == len(s):
-                ans.append(current.copy())
+        def helper(b,curr):
+            print(b,len(s),curr)
+            if b>= len(s):
+                ans.append(curr.copy())
                 return
-            for index in range(start,len(s)):
-                if isPalindrome(start,index):
-                    current.append(s[start:index+1])
-                    helper(current,index + 1)
-                    current.pop()
-      
-        helper([],0)
-
+            #End of current partition and beginning of next partition
+            for index in range(b+1, len(s) + 1):
+                c = s[b:index]
+                #Check if the word is a palindrome
+                i = 0
+                j = len(c) - 1
+                palindrome = False
+                while i <= j:
+                    if c[i] != c[j]:
+                        break
+                    i+=1
+                    j-=1
+                    if i > j:
+                        palindrome = True
+                if palindrome:
+                    curr.append(c)
+                    helper(index, curr)
+                    curr.pop()
+        helper(0,[])
         return ans
-                
-
-
-                
-
-        
-
-
-
-        
-
