eab6356fed43ba43e39b3cef5c62e64837673310
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-05-10 18:47:01 -0400

    Time: 469 ms (50.90%) | Memory: 35.2 MB (66.84%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -5,17 +5,18 @@ class Solution:
         #Every partition should not move to the next size unless the current size is a palindrome
         ans = []
         def helper(b,curr):
-            print(b,len(s),curr)
             if b>= len(s):
                 ans.append(curr.copy())
                 return
             #End of current partition and beginning of next partition
             for index in range(b+1, len(s) + 1):
+                #Create a subword
                 c = s[b:index]
-                #Check if the word is a palindrome
+                #Put a pointer at the beginning of subword and end of subword
                 i = 0
                 j = len(c) - 1
                 palindrome = False
+                #Check if the word is a palindrome
                 while i <= j:
                     if c[i] != c[j]:
                         break
