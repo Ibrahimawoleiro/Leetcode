9e532ea8922626d3737a899e7dff3d7f1692352a
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-05-10 18:53:56 -0400

    Time: 466 ms (58.28%) | Memory: 35.1 MB (66.84%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -8,24 +8,27 @@ class Solution:
             if b>= len(s):
                 ans.append(curr.copy())
                 return
+            #Check subarrays starting from beginning to end of arr
             #End of current partition and beginning of next partition
             for index in range(b+1, len(s) + 1):
                 #Create a subword
-                c = s[b:index]
+                
                 #Put a pointer at the beginning of subword and end of subword
-                i = 0
-                j = len(c) - 1
+                i = b
+                j = index - 1
                 palindrome = False
                 #Check if the word is a palindrome
                 while i <= j:
-                    if c[i] != c[j]:
+                    if s[i] != s[j]:
                         break
                     i+=1
                     j-=1
                     if i > j:
                         palindrome = True
+                #If it is a palindrome, append it to current
+                #Then check different combination for the right side
                 if palindrome:
-                    curr.append(c)
+                    curr.append(s[b:index])
                     helper(index, curr)
                     curr.pop()
         helper(0,[])
