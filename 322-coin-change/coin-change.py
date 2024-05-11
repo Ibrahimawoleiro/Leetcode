39ef4277ef87adb3b2ec77d07d3b6565570fd89a
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-05-11 09:43:33 -0400

    Time: 1032 ms (23.62%) | Memory: 44.1 MB (9.04%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,28 +1,28 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        dic = {}
 
-        dp = {}
-
-        def helper(curr):
-            if curr < 0:
-                return curr
-            
-            if curr == 0:
+        def helper(rem):
+            if rem == 0:
                 return 0
-            
-            if curr in dp:
-                return dp[curr]
-
-            smallest = 2 ** 33
-
-            for val in coins:
-                path = helper(curr - val)
-                if path >=0 and  path < smallest:
-                    smallest = path
-            if smallest == 2 ** 33:
-                dp[curr] = -1
+            elif rem < 0:
                 return -1
-            dp[curr] = 1 + smallest
-            return dp[curr]
+            
+            if rem in dic:
+                return dic[rem]
+
+            minimum = -1
+
+            for coin in coins:
+                remainder = rem - coin
+                res = helper(remainder)
+                if res >= 0:
+                    if minimum > 0:
+                        minimum = min(minimum, res + 1)
+                    else:
+                        minimum = res + 1
+                    
+            dic[rem] = minimum
+            return minimum
 
         return helper(amount)
