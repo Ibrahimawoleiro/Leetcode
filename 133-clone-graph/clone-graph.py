ea36d354b8922b83a757251519769c3432c8f2b9
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-05-11 17:38:15 -0400

    Time: 41 ms (52.80%) | Memory: 17.2 MB (9.25%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -9,16 +9,72 @@ class Node:
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return node
+        elif len(node.neighbors) == 0:
+            return Node(node.val)
+        dic = {}
+        dic_neighbors = {}
+        visited = set()
+        def helper(node):
+            if node in visited:
+                return
+            visited.add(node)
+            for neighbor in node.neighbors:
+                #If the neigbor is already in seen neighbors of node.val
+                if node.val in dic_neighbors:
+                    if neighbor.val in dic_neighbors[node.val]:
+                        continue
+                    #If the record of the node is not in the dic
+                if node.val not in dic:
+                    #Create a new storage for current
+                    dic[node.val] = []
+                    dic[node.val].append(neighbor.val)
+                    dic_neighbors[node.val] = set()
+                    dic_neighbors[node.val].add(neighbor.val)
+                    #If the neigbor isn't present in dic, create a new storage for it 
+                    if neighbor.val not in dic:
+                        dic[neighbor.val] = []
+                        dic[neighbor.val].append(node.val)
+                        dic_neighbors[neighbor.val] = set()
+                        dic_neighbors[neighbor.val].add(node.val)
+                    #If the neighbor is already in dic
+                    else:
+                        dic[neighbor.val].append(node.val)
+                        dic_neighbors[neighbor.val].add(node.val)
+
+
+                #If the record of the node is already in the dic
+                else:
+                    dic[node.val].append(neighbor.val)
+                    dic_neighbors[node.val].add(neighbor.val)
+
+                    #If the neigbor isn't present in dic, create a new storage for it 
+                    if neighbor.val not in dic:
+                        dic[neighbor.val] = []
+                        dic[neighbor.val].append(node.val)
+                        dic_neighbors[neighbor.val] = set()
+                        dic_neighbors[neighbor.val].add(node.val)
+                    else:
+                        dic[neighbor.val].append(node.val)
+                        dic_neighbors[neighbor.val].add(node.val)
+                helper(neighbor)
+        helper(node)
+        #Now we model the relationships
         seen = {}
-        def helper(current,copy):
-            if not current:
-                return 
-            print(current.val)
-            if current.val in seen:
-                return seen[current.val]
-            copy = Node(current.val, neighbors = [])
-            seen[current.val] = copy
-            for index in current.neighbors:
-                copy.neighbors.append(helper(index,None))
-            return copy
-        return helper(node,None)
+        
+        def joiner(val):
+            if val in seen:
+                return seen[val]
+            curr = Node(val, [])
+            seen[val]=curr
+            for n in dic[val]:
+                curr.neighbors.append(joiner(n))
+            return curr
+
+        
+
+        print("visited",visited)
+        print("dic",dic)
+        print("dic_neighbor",dic_neighbors)
+        return joiner(1)
