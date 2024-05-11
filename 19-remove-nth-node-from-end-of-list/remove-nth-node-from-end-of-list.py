968f461274f91892066c8f73042c351d1fa5ef40
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-05-11 16:03:04 -0400

    Time: 34 ms (75.00%) | Memory: 16.7 MB (7.17%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -5,43 +5,27 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        #Approach 1
-        # if head.next == None and n>0:
-        #     return None
-        # result = ListNode()
-        # result.next = head
-        # fast = result
-        # slow = result
-
-        # while fast != None:
-        #     if n+1 > 0:
-        #         fast = fast.next
-        #         n-=1
-        #     else:
-        #         fast = fast.next
-        #         slow = slow.next
-
-        # if slow.next!=None:
-        #     slow.next = slow.next.next
+        #Stall the back pointer for n number of times
+        #If at this point fast is on None:
+        #return back.next
+        #Then move both back and front pointer
+        #Keep a previous pointer that points to front of back
         
-        # return result.next
+        back = head
+        front = head
+        previous = None
 
-        #Approach 2
-        delayed = ListNode()
-        ans = delayed
-        delayed.next = head
-        runner = head
+        while(n > 0):
+            n -= 1
+            front = front.next
+        
+        if front == None:
+            return back.next
+        while(front!= None):
+            front = front.next
+            previous = back
+            back = back.next
+        
+        previous.next = previous.next.next
 
-        while(runner):
-            if n > 0:
-                runner = runner.next
-                n-=1
-                continue
-            runner = runner.next
-            delayed = delayed.next
-
-        delayed.next = delayed.next.next
-
-        return ans.next
-            
-
+        return head
