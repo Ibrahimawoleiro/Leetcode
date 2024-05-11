25e071ad94bfcc89d2c1bdfee1fbc437a4723f17
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-05-10 20:15:13 -0400

    Time: 28 ms (93.31%) | Memory: 16.6 MB (44.28%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,39 +1,29 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # #Approach 1
-        # if len(nums) == 1:
-        #     return nums[0]
-        
-        # dp = {}
+        dic = {}
+        def helper(i):
+            if i >= len(nums):
+                return 0
 
-        # def helper(starter):
-        #     if starter >= len(nums) - 1:
-        #         if starter == len(nums) - 1:
-        #             return nums[starter]
-        #         else:
-        #             return 0
-        #     if starter in dp:
-        #         return dp[starter]
-        #     for index in range(starter,len(nums)):
-        #         total = nums[index]
-        #         for house in range(index + 2, len(nums)):
-        #             curr_neighbor = helper(house)
-        #             if nums[index] + curr_neighbor > total:
-        #                 total = nums[index] + curr_neighbor
-        #         dp[index] = total
-        #     return dp[starter]
-       
-        # helper(0)
-        # return max(dp[0] if 0 in dp else 0, dp[1] if 1 in dp else 0)
+            if i in dic:
+                return dic[i]
+            m = 0
+            for r in range(i+2, len(nums)):
+                #right neighbor
+                n = helper(r)
+                if n > m:
+                    m = n
+            dic[i] = m + nums[i]
+
+            return dic[i]
+        helper(0)
+        helper(1)
+        print(dic)
+        if len(nums) > 1:
+            if 1 in dic:
+                return max(dic[0], dic[1])
+        return dic[0]
         
 
-        #Approach 2
-        if len(nums) == 1:
-            return nums[0]
-        for index in reversed(range(len(nums))):
-            if index == len(nums) - 1:
-                continue
-            nums[index] = max(nums[index] + (nums[index + 2] if index + 2 < len(nums) else 0),nums[index + 1])
-        return max(nums[0],nums[1])
 
-         
+
