d556f83b8319d97e691f2d9b57ffd266f9cc51e3
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-05-12 16:54:37 -0400

    Time: 43 ms (57.42%) | Memory: 17.1 MB (14.98%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,22 +1,39 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # #Brute force
-        # for index in range(len(nums)):
-        #     if nums[index] == target:
-        #         return index
-        # return -1
+        def bs(s, e):
+            while(s<= e):
+                mid = (s + e)// 2
+                if nums[mid] == target:
+                    return mid
+                elif nums[mid] > target:
+                    e = mid - 1
+                else:
+                    s = mid + 1
+            return -1
 
-        #Cases: 
-        l = 0
-        r = len(nums) - 1
+        def rec(s, e):
+            print(s,e)
+            
+            if s > e:
+                return -1
+            
+            mid = (s + e)//2
 
-        while(l <= r):
-            if nums[l] == target:
-                return l
-            elif nums[r] == target:
-                return r
-            if target > nums[0]:
-                r -= 1
-            elif target < nums[0]:
-                l += 1
-        return -1 
+            if nums[mid] == target:
+                return mid
+            print(nums[s],nums[mid], nums[e])
+            print()
+            #If the target might be located in the sorted part
+            if nums[mid] < nums[e] and target <= nums[e] and target >= nums[mid]:
+                return bs(mid + 1, e)
+            elif nums[mid] > nums[s] and target >= nums[s] and target < nums[mid]:
+                print('k')
+                return bs(s, mid - 1)
+            #If the target might be located in one of the unsorted part
+            elif nums[mid] < nums[s] and (target < nums[mid] or target >= nums[s]):
+                return rec(s, mid - 1)
+            elif nums[mid] > nums[e] and (target <= nums[e] or target > nums[mid]):
+                return rec(mid + 1, e)
+            return -1
+        return rec(0, len(nums) - 1)
+            
