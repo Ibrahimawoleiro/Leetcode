5aea89ee299181a25fdb31c5efa5bca672b4a30e
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-05-12 16:55:04 -0400

    Time: 41 ms (73.05%) | Memory: 17 MB (14.98%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -12,7 +12,6 @@ class Solution:
             return -1
 
         def rec(s, e):
-            print(s,e)
             
             if s > e:
                 return -1
@@ -21,19 +20,18 @@ class Solution:
 
             if nums[mid] == target:
                 return mid
-            print(nums[s],nums[mid], nums[e])
-            print()
             #If the target might be located in the sorted part
             if nums[mid] < nums[e] and target <= nums[e] and target >= nums[mid]:
                 return bs(mid + 1, e)
             elif nums[mid] > nums[s] and target >= nums[s] and target < nums[mid]:
-                print('k')
                 return bs(s, mid - 1)
             #If the target might be located in one of the unsorted part
             elif nums[mid] < nums[s] and (target < nums[mid] or target >= nums[s]):
                 return rec(s, mid - 1)
+                
             elif nums[mid] > nums[e] and (target <= nums[e] or target > nums[mid]):
                 return rec(mid + 1, e)
+
             return -1
         return rec(0, len(nums) - 1)
             
