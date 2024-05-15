78c15589798e8fbbaf007ccfc2b4a2a7a3406568
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-05-15 17:45:35 -0400

    Time: 89 ms (5.09%) | Memory: 21.2 MB (7.28%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
@@ -5,20 +5,28 @@
 #         self.left = left
 #         self.right = right
 
-import heapq
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        heap = []
-        self.helper(root, heap)
-        return heapq.heappop(heap) * -1
+        ans = [-float('inf')]
+        self.helper(root, ans)
+        return ans[0]
+      
     
-    def helper(self, node, heap):
+    def helper(self, node,ans):
         if not node:
             return 0
 
-        l = self.helper(node.left,heap)
-        r = self.helper(node.right, heap)
+        l = self.helper(node.left,ans)
+        r = self.helper(node.right,ans)
+
         curr = l + r + node.val
         curr_only = node.val
-        heapq.heappush(heap, min(min(-curr, -node.val),-(node.val + max(l, r))))
-        return max(node.val + max(l, r), node.val)
+        l_and_curr = node.val + l
+        r_and_curr = node.val + r
+        
+        result = max(curr, max(max(curr_only, l_and_curr), r_and_curr))
+        ans[0] = max(result, ans[0])
+        print(result, ans[0])
+        return max(max(l_and_curr, r_and_curr),curr_only)
+        # heapq.heappush(heap, min(min(-curr, -node.val),-(node.val + max(l, r))))
+        # return max(node.val + max(l, r), node.val)
