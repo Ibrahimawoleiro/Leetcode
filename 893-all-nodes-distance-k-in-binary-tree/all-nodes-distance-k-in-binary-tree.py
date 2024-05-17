15b651b55c954dd93ae03e54f03b8d0af0244f93
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-05-17 19:28:38 -0400

    Time: 30 ms (96.09%) | Memory: 17.3 MB (11.03%) - LeetSync

---
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+import queue
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        #Get the parent of each node into a map
+        
+        #When you are trying to get the  values away, You have three options
+        #Go up
+        #Go down left
+        #Go down right
+        ans = []
+        visited = set()
+        def helper(curr, d):
+            print(curr.val)
+            if d > k or curr in visited:
+                return 
+            if d == k:
+                ans.append(curr.val)
+            visited.add(curr)
+            if curr.left:
+                helper(curr.left,d + 1)
+            if curr.right:
+                helper(curr.right, d + 1)
+            if curr in dic:
+                helper(dic[curr], d+ 1)
+            
+
+        dic = {}
+        q = queue.Queue()
+        q.put(root)
+        
+        start = target
+        while not q.empty():
+            curr = q.get()
+            #Map children to their parents
+            if curr.left:
+                dic[curr.left] = curr
+                q.put(curr.left)
+            if curr.right:
+                dic[curr.right] = curr
+                q.put(curr.right)
+        
+        helper(target, 0)
+        return ans
