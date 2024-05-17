754b8fb2cf8addb1df1a7630c6fb586207b74bf9
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-05-16 23:08:08 -0400

    Time: 131 ms (6.27%) | Memory: 22 MB (5.21%) - LeetSync

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,47 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        dic = {}
+        dic[p.val] = False
+        dic[q.val] = False
+
+        return self.helper(root, p,q,dic)[2][0]
+
+    def helper(self,root, p, q, dic):
+        if not root:
+            return [False, False, [None, False]]
+        l = self.helper(root.left, p, q, dic)
+
+        r = self.helper(root.right, p, q, dic)
+        print(root.val)
+        print(l, root.val)
+        print(r, root.val)
+        if l[2][1]:
+            return l
+        if r[2][1]:
+            return r
+        if l[0] and r[1] or l[1] and r[0]:
+            return [True,True,[root,True]]
+        if p.val == root.val:
+            print(p.val,'xzcvghbjkl;kjhcxvbn')
+            if l[1]:
+                return [True, True, [root, True]]
+            elif r[1]:
+                print("yujlkhgfcgjkcvjkliuhguyhuhuihuhiuhuiuihijkjikjnjijni")
+                return [True, True, [root, True]]
+        elif q.val == root.val:
+            if l[0]:
+                return [True, True, [root, True]]
+            elif r[0]:
+                return [True, True, [root, True]]
+        
+        print(l[0], r[0], r[1], l[1])
+        print( True if root.val == p.val else False, True if root.val == q.val else False)
+        return [ True if root.val == p.val or l[0] or r[0] else False, True if root.val == q.val or l[1] or r[1] else False, [None, False]]
+
