55172695bd6d01a0cefef76419401979bfeea2d0
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-05-19 19:50:06 -0400

    Time: 66 ms (29.24%) | Memory: 19.7 MB (11.29%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -2,19 +2,17 @@ class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         store = {}
         def helper(r,i, store):
-            if r >= len(triangle):
+            if r >= len(triangle) or i >= len(triangle[r]):
                 return 0
             if (r, i) in store:
                 return store[(r, i)]
-            for index in range(i,i + 1):
-                curr = triangle[r][index]
-                min_path = curr + min(helper(r+1, i,store), helper(r+1, i+1,store))
-                store[(r, index)] = min_path
+            curr = triangle[r][i]
+            min_path = curr + min(helper(r+1, i,store), helper(r+1, i+1,store))
+            store[(r, i)] = min_path
 
             return store[(r, i)]
 
         ans = helper(0, 0, store)
-        print(store)
         return ans
 
         # [-1],
