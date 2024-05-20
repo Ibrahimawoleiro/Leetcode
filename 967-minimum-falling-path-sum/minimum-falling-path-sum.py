96be74424959159bfa1fa48c5007889bb9437e28
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-05-19 21:04:55 -0400

    Time: 247 ms (5.01%) | Memory: 19.5 MB (18.43%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        store = {}
+        min_sum = float('inf')
+
+        def helper(c,store):
+            if c[0] >= len(matrix) or c[0] < 0 or c[1] >= len(matrix[0]) or c[1] < 0:
+                return float('inf')
+            if c in store:
+                return store[c]
+            if c[0] == len(matrix) - 1:
+                store[c] = matrix[c[0]][c[1]]
+                return matrix[c[0]][c[1]]
+            curr = matrix[c[0]][c[1]] + min(min(helper((c[0] + 1, c[1] + 1), store), helper((c[0] + 1, c[1]),store))         , helper((c[0] + 1, c[1] - 1), store) )
+            store[c] = curr
+            print(c, curr)
+            return curr
+
+        for c_i in range(len(matrix[0])):
+            curr = helper((0, c_i), store)
+            min_sum = min(curr, min_sum)
+        print(min_sum)
+        print(store)
+        return min_sum
