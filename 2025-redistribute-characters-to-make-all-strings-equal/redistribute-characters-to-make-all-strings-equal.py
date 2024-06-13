2821d66bdeca760b17ae13e94157dbbe4e25defe
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-06-13 19:12:13 -0400

    Time: 70 ms (16.85%) | Memory: 16.7 MB (82.51%) - LeetSync

---
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.py
@@ -0,0 +1,16 @@
+class Solution:
+    def makeEqual(self, words: List[str]) -> bool:
+        if len(words) == 1:
+            return True
+        store = {}
+        for arr in words:
+            for l in arr:
+                if l in store:
+                    store[l] += 1
+                else:
+                    store[l] = 1
+        print(store)
+        for (key, val) in store.items():
+            if val % len(words) > 0:
+                return False
+        return True
