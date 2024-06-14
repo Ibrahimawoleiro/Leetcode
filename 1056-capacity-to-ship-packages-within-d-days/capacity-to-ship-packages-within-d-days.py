1810ae6c19d46b90f3f28fd452a2dec80adac96f
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-06-14 14:24:30 -0400

    Time: 623 ms (6.27%) | Memory: 22.2 MB (54.66%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,36 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        l = 1
+        h = sum(weights)
+        ans = 10 ** 10
+        while l <= h:
+            mid = (l + h) // 2
+            total = 0
+            count = 0
+            ReachEnd = True
+            for i in range(len(weights)):
+                if weights[i] > mid:
+                    ReachEnd = False
+                    break
+                total += weights[i]
+                if total >= mid:
+                    if total > mid:
+                        total = weights[i]
+                    else:
+                        total = 0
+                    count += 1
+                if total > 0 and i == len(weights) - 1:
+                    count += 1
+            print(count)
+            print(mid)
+            if not ReachEnd:
+                l = mid + 1
+                continue
+            if count <= days:
+                if mid < ans:
+                    ans = mid
+                h = mid - 1
+            else:
+                l = mid + 1
+
+        return ans
