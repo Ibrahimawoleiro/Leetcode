f23c2b76b2d9af7feeb7d1fac1159e8b209c7d9d
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-06-14 16:01:49 -0400

    Time: 176 ms (6.85%) | Memory: 21.7 MB (8.90%) - LeetSync

---
+++ 554-brick-wall/brick-wall.py
@@ -0,0 +1,20 @@
+class Solution:
+    def leastBricks(self, wall: List[List[int]]) -> int:
+        heap = []
+        store = {}
+        for r_i in range(len(wall)):
+            total = 0
+            for c_i in range(len(wall[r_i]) -  1):
+                print(r_i, c_i)
+                total += wall[r_i][c_i]
+                if total not in store:
+                    store[total] = 1
+                else:
+                    store[total] += 1
+        top = 0
+        print(top, store)
+        for val in store.values():
+            if val > top:
+                top = val
+
+        return len(wall) - top
