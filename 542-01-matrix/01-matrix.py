81fa41dedaf3006fa5612ab48ffdfa604f14be0d
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-06-18 00:33:23 -0400

    Time: 917 ms (5.01%) | Memory: 21.3 MB (5.86%) - LeetSync

---
+++ 542-01-matrix/01-matrix.py
@@ -0,0 +1,40 @@
+import queue
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        ans = [[0 for num in range(len(mat[0]))] for val in range(len(mat))]
+        seen = set()
+        q = queue.Queue()
+        for r in range(len(mat)):
+            for c in range(len(mat[0])):
+                if mat[r][c] == 0:
+                    q.put((r,c))
+                    seen.add((r,c))
+        q.put(None)
+        d = 0
+        print(seen)
+        print(d)
+        while(not q.empty()):
+            curr = q.get()
+            if curr:
+                r = curr[0]
+                c = curr[1]
+                ans[r][c] = d
+                if r - 1 >= 0 and (r - 1, c) not in seen:
+                    q.put((r-1,c))
+                    seen.add((r - 1, c))
+                if r + 1 < len(ans) and (r + 1, c) not in seen:
+                    q.put((r + 1, c))
+                    seen.add((r + 1, c))
+                if c - 1 >= 0 and (r, c - 1) not in seen:
+                    q.put((r, c - 1))
+                    seen.add((r, c - 1))
+                if c + 1 < len(ans[0]) and (r, c + 1) not in seen:
+                    q.put((r, c + 1))
+                    seen.add((r, c + 1))
+            else:
+                # print(seen)
+                if not q.empty():
+                    d += 1
+                    # print(d)
+                    q.put(None)
+        return ans
