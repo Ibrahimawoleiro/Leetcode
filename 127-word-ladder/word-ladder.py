18eea5588b194ce0b266b2b259dbdf1add55adb1
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-06-22 15:51:47 -0400

    Time: 281 ms (54.13%) | Memory: 18.3 MB (56.58%) - LeetSync

---
+++ 127-word-ladder/word-ladder.py
@@ -0,0 +1,24 @@
+import queue
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        q = queue.Queue()
+        store = set(wordList)
+        q.put((beginWord, 1))
+        count = 0
+        while(not q.empty()):
+            curr = q.get()
+            if curr[0] == endWord:
+                return curr[1]
+            word = list(curr[0])
+            count = curr[1]
+            for i in range(len(word)):
+                l = word[i]
+                for ch in string.ascii_lowercase:
+                    word[i] = ch
+                    result = ''.join(word)
+                    if result in store:
+                        q.put((result, count+1))
+                        store.remove(result)
+                word[i] = l
+        print(count)
+        return 0
