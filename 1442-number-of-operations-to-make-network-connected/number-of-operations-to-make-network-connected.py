f20026f4d08925d35ac628c6658051703b68d5a3
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-06-27 14:41:28 -0400

    Time: 437 ms (11.90%) | Memory: 36.5 MB (69.73%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
@@ -0,0 +1,33 @@
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        
+
+        con_comp_count = 0
+
+        graph = [[] for num in range(n)]
+        visited = [0 for num in range(n)]
+        def dfs(curr):
+            print(curr,'iguhsjklvdcx')
+            if visited[curr] == 1:
+                return 
+            visited[curr] = 1
+            
+            for neighbor in graph[curr]:
+                if visited[neighbor ]== 0:
+                    dfs(neighbor)
+
+        for connection in connections:
+            start , end = connection
+            graph[start].append(end)
+            graph[end].append(start) 
+        
+        for num in range(n):
+            if visited[num] != 1:
+                print(num)
+                con_comp_count += 1
+                dfs(num)
+        print(graph)
+        if len(connections) < n - 1:
+            return -1
+
+        return con_comp_count - 1
