7271798f288d9ce3b3edcebf1b90fc3884fe48a4
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-07-04 01:45:26 -0400

    Time: 61 ms (9.21%) | Memory: 16.8 MB (77.71%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,5 +1,27 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_sorted = sorted(s)
-        t_sorted = sorted(t)
-        return s_sorted == t_sorted
+
+        def freq(word, store):
+            for l in word:
+                if l in store:
+                    store[l] += 1
+                else:
+                    store[l] = 1
+        s_store = {}
+        t_store = {}
+
+        freq(s, s_store)
+        freq(t, t_store)
+
+        for l in s:
+            if l not in t_store:
+                return False
+            if t_store[l] != s_store[l]:
+                return False
+        for l in t:
+            if l not in s_store:
+                return False
+            if s_store[l] != t_store[l]:
+                return False
+        return True
+        
