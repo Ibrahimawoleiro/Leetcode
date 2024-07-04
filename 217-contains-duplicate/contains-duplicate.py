574321730f3b059bdd6b1b142e1de1cd0c69f93e
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-07-04 00:39:47 -0400

    Time: 396 ms (97.58%) | Memory: 31.9 MB (75.83%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,32 +1,11 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # #Approach 1
-        # for index in range(len(nums)):
-        #     for checker_index in range(len(nums)):
-        #         if(index == checker_index):
-        #             continue
-        #         if nums[index] == nums[checker_index]:
-        #             return True
-            
-        # return False
-
-        # #Approach 2
-
-        # nums.sort()
-        # for index in range(len(nums)):
-        #     if index == 0:
-        #         continue
-        #     if nums[index] == nums[index - 1]:
-        #         return True
         
-        # return False
+        store = set()
 
-        #Approach 3
-        checker = set()
-
-        for number in nums:
-            if number not in checker:
-                checker.add(number)
-            else:
+        for num in nums:
+            if num in store:
                 return True
+            store.add(num)
+
         return False
