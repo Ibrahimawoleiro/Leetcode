8443df336503fd1663ece1eeb62028e7115d2d80
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-07-04 19:37:42 -0400

    Time: 35 ms (69.94%) | Memory: 16.4 MB (96.93%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -2,15 +2,21 @@ class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         
         last = [1]
+        if numRows == 1:
+            return [last]
         ans = []
-        for j in range(2, numRows+2):
+        for j in range(2, numRows+1):
             curr = [0 for num in range(j)]
             for i in range(j):
                 if i == 0 or i == j - 1:
                     curr[i] = 1
                 else:
                     curr[i] = last[i - 1] + last[i]
-            ans.append(last.copy())
+            if j == 2:
+                ans.append(last.copy())
+                ans.append(curr.copy())
+            else:
+                ans.append(curr.copy())
             last = curr
 
         return ans
