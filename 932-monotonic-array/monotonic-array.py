c599e512722b9d25defdaf54d0499524106eabf1
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-07-04 23:17:59 -0400

    Time: 724 ms (51.48%) | Memory: 31.3 MB (37.68%) - LeetSync

--- 932-monotonic-array/monotonic-array.py
+++ 932-monotonic-array/monotonic-array.py
@@ -1,28 +1,12 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
-        if len(nums) == 1:
-            return True
-        i = 0
-        j = 1
-        while(j < len(nums)):
-            if nums[i] == nums[j]:
-                i+=1
-                j+=1
-            elif nums[i] < nums[j]:
-                i+=1
-                j+=1
-                while(j < len(nums)):
-                    if nums[i] > nums[j]:
-                        return False
-                    i+=1
-                    j+=1
-            else:
-                i+=1
-                j+=1
-                while(j < len(nums)):
-                    if nums[i] < nums[j]:
-                        return False
-                    i+=1
-                    j+=1
+        increasing = False
+        decreasing = False
 
-        return True
+        for index in range(1,len(nums)):
+            if nums[index] > nums[index - 1]:
+                increasing = True
+            if nums[index] < nums[index - 1]:
+                decreasing = True
+
+        return not(increasing and decreasing) 
