30fbcdab28d32e0b0bc1ec5f2705dafc1eb4a0a3
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-07-05 12:16:09 -0400

    Time: 37 ms (75.46%) | Memory: 16.6 MB (35.02%) - LeetSync

---
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        
+        c = 0
+        s = 0
+
+        for val in students:
+            if val == 0:
+                c+=1
+            
+            else:
+                s += 1
+        
+        print(s, c)
+        for val in sandwiches:
+            if val == 0 and c == 0 or val == 1 and s == 0:
+                return c + s
+            
+            elif val == 0:
+                c -= 1
+            else:
+                s -= 1
+        return c + s
