9d45eb1dc454ac2e3519de6d830d8cf48303464d
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-07-11 09:53:00 -0400

    Time: 122 ms (14.85%) | Memory: 18.9 MB (24.57%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        opening_count = 0
+        closing_count = 0
+        count = 0
+        ans = ''
+        helper = []
+
+        for i in range(len(s)):
+            if (s[i] != ')') or s[i] == ')' and count > 0:
+                helper.append(s[i])
+                if s[i] == '(':
+                    opening_count += 1
+                    count += 1
+
+                elif s[i] == ')':
+                    closing_count += 1
+                    count -= 1
+        
+        o = 0
+        c = 0
+        print(closing_count)
+        for char in helper:
+            if char == '(' and o < closing_count or char == ')' and c < closing_count:
+                ans += char
+                if char =='(':
+                    o += 1
+                if char == ')':
+                    c += 1
+            elif char != '(' and char != ')':
+                ans += char
+
+        return ans
