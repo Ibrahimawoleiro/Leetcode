01fc81bbb2338bd9e478171c04cd81ce2245ad23
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-07-19 12:31:51 -0400

    Time: 712 ms (75.60%) | Memory: 31.1 MB (5.02%) - LeetSync

---
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.py
@@ -0,0 +1,31 @@
+import heapq
+class Solution:
+    def minimizeMax(self, nums: List[int], p: int) -> int:
+        
+        nums.sort()
+        #[1,2,2,4]
+        i = 0
+        j = nums[len(nums) - 1] - nums[0]
+        m = 10** 10
+        while i <= j:
+
+            mid = (i + j) // 2
+            count = 0
+            k = 1
+            while k < len(nums):
+
+                if nums[k] - nums[k - 1] <= mid:
+                    count += 1
+                    k += 2
+                else:
+                    k += 1
+            print('huiojlfewsd')
+            print(mid, m, 'count', count)
+            if count >= p:
+                if mid < m:
+                    m = mid
+                    j = mid - 1
+            else:
+                i = mid + 1
+
+        return m
