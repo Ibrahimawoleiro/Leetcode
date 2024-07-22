1125fe252d4ace84e39f7110a8c9435476208476
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-07-21 23:10:01 -0400

    Time: 814 ms (5.34%) | Memory: 22.9 MB (26.72%) - LeetSync

---
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.swift
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func pairSum(_ head: ListNode?) -> Int {
+        var reverse: (ListNode?) -> ListNode? = { (curr: ListNode?) in
+            guard let curr = curr else {return nil};
+            var current : ListNode? = curr;
+            var prev: ListNode? = nil;
+            var next: ListNode? = nil;
+            while current != nil{
+                next = current?.next;
+                current?.next = prev;
+                prev = current;
+                current = next;
+            }
+            return prev;
+        }
+
+        var fast: ListNode? = head;
+        var slow: ListNode? = head;
+        var prev: ListNode? = head;
+        while fast != nil{
+            prev = slow;
+            fast = fast?.next?.next;
+            slow = slow?.next;
+        }
+        if let prev = prev{
+            prev.next = nil;
+        }
+
+        slow = reverse(slow);
+
+        
+
+        var maximum : Int = 0;
+        fast = head;
+
+        print(slow?.val);
+        print(fast?.val);
+        while fast != nil{
+            
+            guard let slow_node = slow else {break}
+            guard let fast_node = fast else {break}
+            print("uhiesfdk")
+            maximum = max(maximum, (slow_node.val + fast_node.val))
+            fast = fast?.next;
+            slow = slow?.next;
+        }
+
+        return maximum;
+    }
+}
