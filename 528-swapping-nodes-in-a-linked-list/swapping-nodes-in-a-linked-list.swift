07e76d9fb8ec4960b8504aafce6569b79cdbc2d6
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-07-21 23:31:44 -0400

    Time: 478 ms (66.67%) | Memory: 24.7 MB (8.33%) - LeetSync

---
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.swift
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func swapNodes(_ head: ListNode?, _ k: Int) -> ListNode? {
+        
+        var slow : ListNode? = head;
+        var fast : ListNode? = head;
+
+        var left : ListNode? = head;
+        var count: Int = 1;
+        while count != k {
+            left = left?.next;
+            count += 1;
+        }
+        count = 0
+        while count != k{
+            fast = fast?.next;
+            count += 1;
+        }
+
+        while fast != nil{
+            fast = fast?.next;
+            slow = slow?.next;
+        }
+        print(left?.val);
+        print(slow?.val);
+        if let left = left, let right = slow{
+            var temp: Int = left.val;
+            left.val = right.val;
+            right.val = temp;
+        }
+        return head
+    }
+}
