9a2967a7c4e3ce886de4e97d3c0abba6a762784b
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-08-18 15:02:41 -0400

    Time: 0 ms (100.00%) | Memory: 7.4 MB (68.54%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -11,20 +11,51 @@ public:
     //     return helper(n - 1) + helper(n - 2);
     // }
 
-    //Memoization fuction
-
-    int memoization(int n , vector<int>& dp){
-        if (n == 0 || n == 1){
-            return 1;
-        }
-        if (dp[n] != -1){
-            return dp[n];
-        }
+    // Memoization function
+    // TC -> O(N)
+    // SC -> O(N)
+    // int memoization(int n , vector<int>& dp){
+    //     if (n == 0 || n == 1){
+    //         return 1;
+    //     }
+    //     if (dp[n] != -1){
+    //         return dp[n];
+    //     }
         
-        int curr = memoization(n - 1, dp) + memoization(n - 2, dp);
+    //     int curr = memoization(n - 1, dp) + memoization(n - 2, dp);
 
-        dp[n] = curr;
+    //     dp[n] = curr;
 
+    //     return curr;
+    // }
+
+    // Tabulation function
+    // TC -> O(N)
+    // SC -> O(N)
+    // int tabulation(int n){
+    //     vector<int> tab(n + 1, 0);
+    //     tab[0] = 1;
+    //     tab[1] = 1;
+
+    //     for(int i = 2; i <= n; i++){
+    //         tab[i] = tab[i - 1] + tab[i - 2];
+    //     }
+    //     return tab[n];
+    // }
+
+    // OptimizedTabulation function
+    // TC -> O(N)
+    // SC -> O(1)
+
+    int opt_tabulation(int n){
+        int prev = 1;
+        int curr = 1;
+
+        for (int i = 2; i <= n; i ++){
+            int next = curr + prev;
+            prev = curr;
+            curr = next;
+        }
         return curr;
     }
 
@@ -39,7 +70,6 @@ public:
         You can only get to current position from last 2 step or last step.
         */
 
-        vector<int> dp(n + 1, -1);
-        return memoization(n, dp);
+        return opt_tabulation(n);
     }
 };
