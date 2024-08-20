4b7769ce46b3d6bbb1422f2efad85b749aa90d3a
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-08-20 01:37:59 -0400

    Time: 0 ms (100.00%) | Memory: 9.6 MB (9.13%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -15,18 +15,37 @@ public:
     //     return max(pick, not_pick);
     // }
 
-    int memoized(vector<int>& nums, vector<int>& dp,  int index){
-        if (index < 0) return 0;
-        if (index == 0) return nums[0];
-        if (dp[index] != -1) return dp[index];
-        int pick = nums[index] + memoized(nums,dp, index - 2);
-        int not_pick = memoized(nums,dp, index - 1);
-        dp[index] = max(pick, not_pick);
-        return dp[index];
+    // // TC -> O(N)
+    // // SC -> O(N + N)
+    // int memoized(vector<int>& nums, vector<int>& dp,  int index){
+    //     if (index < 0) return 0;
+    //     if (index == 0) return nums[0];
+    //     if (dp[index] != -1) return dp[index];
+    //     int pick = nums[index] + memoized(nums,dp, index - 2);
+    //     int not_pick = memoized(nums,dp, index - 1);
+    //     dp[index] = max(pick, not_pick);
+    //     return dp[index];
+    // }
+
+    //
+    int tabulation(vector<int> nums){
+        int size = nums.size();
+        vector<int> dp(size, -1);
+        dp[0] = nums[0];
+        for (int i = 1; i<= size - 1; i++){
+            int not_pick = dp[i - 1];
+            int pick = nums[i];
+            if (i - 2 >= 0){
+                pick = nums[i] + dp[i - 2];
+            }
+
+            dp[i] = max(not_pick, pick);
+        }
+
+        return dp[size - 1];
     }
 
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        return memoized(nums,dp, nums.size() - 1);
+        return tabulation(nums);
     }
 };
