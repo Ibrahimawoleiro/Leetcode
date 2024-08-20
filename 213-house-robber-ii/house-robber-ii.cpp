47199554016d24e86a98d8183ab080a0b79d881d
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-08-20 15:25:29 -0400

    Time: 0 ms (100.00%) | Memory: 9.7 MB (25.36%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -16,30 +16,45 @@ public:
     //     return max(not_take, take);
     // }
 
-    // TC -> O(N)
-    // SC -> O(N + N)
-    int memoized(vector<int>& nums,vector<int>& dp, int index){
-        if (index < 0) return 0;
-        if (index == 0) return nums[0];
-        if (dp[index] != -1) return dp[index];
+    // // TC -> O(N)
+    // // SC -> O(N + N)
+    // int memoized(vector<int>& nums,vector<int>& dp, int index){
+    //     if (index < 0) return 0;
+    //     if (index == 0) return nums[0];
+    //     if (dp[index] != -1) return dp[index];
 
-        int not_take = memoized(nums, dp, index - 1);
+    //     int not_take = memoized(nums, dp, index - 1);
 
-        int take = nums[index] +  memoized(nums, dp, index - 2);
+    //     int take = nums[index] +  memoized(nums, dp, index - 2);
 
-        dp[index] = max(not_take, take);
+    //     dp[index] = max(not_take, take);
 
-        return dp[index];
+    //     return dp[index];
+    // }
+
+    int tabulation(vector<int>& nums){
+        vector<int> dp(nums.size(), -1);
+        dp[0] = nums[0];
+
+        for(int i = 1; i < nums.size(); i++){
+
+            int not_take = dp[i - 1];
+            int take = nums[i];
+            if (i - 2 >= 0){
+                take = nums[i] + dp[i - 2];
+            }
+
+            dp[i] = max(take, not_take);
+        }
+
+        return dp[nums.size() - 1];
     }
-
     int rob(vector<int>& nums) {
         if (nums.size() == 1){
             return nums[0];
         }
         vector<int> omit_f(nums.begin()+1, nums.end());
         vector<int> omit_l(nums.begin(), nums.begin() + nums.size() - 1);
-        vector<int> dp_f(nums.size(), -1);
-        vector<int> dp_l(nums.size(), -1);
-        return max(memoized(omit_f, dp_f, omit_f.size() - 1), memoized(omit_l, dp_l,  omit_l.size() - 1));
+        return max(tabulation(omit_f), tabulation(omit_l));
     }
 };
