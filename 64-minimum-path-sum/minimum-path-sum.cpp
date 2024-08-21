ec6cfb8bcab5ddf01115c07265b31f788b786985
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-08-21 14:13:31 -0400

    Time: 3 ms (95.26%) | Memory: 12.8 MB (55.57%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -32,30 +32,57 @@ public:
     //     return dp[r][c];
     // }
 
-    int tabulation(vector<vector<int>>& grid){
-        vector<vector<int>> dp(grid.size(), vector<int>(grid[0].size(), -1));
+    // // TC -> O(N * M * 2)
+    // // SC -> O(N * M)
+    // int tabulation(vector<vector<int>>& grid){
+    //     vector<vector<int>> dp(grid.size(), vector<int>(grid[0].size(), -1));
+    //     for (int r = 0; r < grid.size(); r++){
+    //         for (int c = 0; c < grid[0].size(); c++){
+    //             if (r == 0 && c == 0){
+    //                 dp[r][c] = grid[0][0];
+    //                 continue;
+    //             }
+    //             int left = 100000000;
+    //             int up = 1000000000;
+    //             if (c - 1 >= 0){
+    //                 left = dp[r][c - 1];
+    //             }
+    //             if (r - 1 >= 0){
+    //                 up = dp[r - 1][c];
+    //             }
+    //             dp[r][c] = grid[r][c] + min(up, left);
+    //         }
+    //     }
+    //     return dp[grid.size() - 1][grid[0].size() - 1];
+    // }
+
+    int optimizedTabulation(vector<vector<int>>& grid){
+        vector<int> prev(grid[0].size(), -1);
+        vector<int> curr(grid[0].size(), -1);
         for (int r = 0; r < grid.size(); r++){
-            for (int c = 0; c < grid[0].size(); c++){
+            for(int c = 0; c < grid[0].size(); c++){
                 if (r == 0 && c == 0){
-                    dp[r][c] = grid[0][0];
+                    curr[c] = grid[r][c];
                     continue;
                 }
                 int left = 100000000;
-                int up = 1000000000;
+                int up = 100000000;
                 if (c - 1 >= 0){
-                    left = dp[r][c - 1];
+                    left = curr[c - 1];
                 }
                 if (r - 1 >= 0){
-                    up = dp[r - 1][c];
+                    up = prev[c];
                 }
-                dp[r][c] = grid[r][c] + min(up, left);
+                curr[c] = grid[r][c] + min(up, left);
             }
+            prev = curr;
+            curr = vector<int>(grid[0].size(), -1);
         }
-        return dp[grid.size() - 1][grid[0].size() - 1];
+        return prev[grid[0].size() - 1];
     }
     
     int minPathSum(vector<vector<int>>& grid) {
         
-        return tabulation(grid);
+        return optimizedTabulation(grid);
     }
 };
