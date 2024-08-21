6af9c5c30398c66e426df17bd5cbf0b0f21291f6
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-08-21 00:50:36 -0400

    Time: 0 ms (100.00%) | Memory: 8.1 MB (15.25%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     // // TC -> O(2 ^ N)
-    // // SC -> O(path)
+    // // SC -> O(N + M)
     // int recursive(int r, int c){
     //     if (r < 0 or c < 0){
     //         return 0;
@@ -15,28 +15,51 @@ public:
     //     return left + up;
     // }
 
-    int memoized(int r, int c, vector<vector<int>>& dp){
-        if (r < 0 || c < 0){
-            return 0;
+    // // TC -> O(N * M)
+    // // SC -> O((N + M) * (N * M))
+    // int memoized(int r, int c, vector<vector<int>>& dp){
+    //     if (r < 0 || c < 0){
+    //         return 0;
+    //     }
+    //     if (r == 0 and c == 0){
+    //         return 1;
+    //     }
+
+    //     if (dp[r][c] != -1){
+    //         return dp[r][c];
+    //     }
+
+    //     int left = memoized(r , c - 1, dp);
+    //     int up = memoized(r - 1, c, dp);
+
+    //     dp[r][c] = left + up;
+
+    //     return dp[r][c];
+    // }
+
+    int tabulation(int m, int n){
+        vector<vector<int>> dp(m, vector<int>(n, -1));  
+        for(int r = 0; r < m; r++){
+            for (int c = 0; c < n; c++){
+                if (r == 0 and c == 0){
+                    dp[r][c] = 1;
+                    continue;
+                }
+                int left = 0;
+                int up = 0;
+
+                if (c - 1 >= 0){
+                    left = dp[r][c - 1];
+                }
+                if (r - 1 >= 0){
+                    up = dp[r - 1][c];
+                }
+                dp[r][c] = left + up;
+             }
         }
-        if (r == 0 and c == 0){
-            return 1;
-        }
-
-        if (dp[r][c] != -1){
-            return dp[r][c];
-        }
-
-        int left = memoized(r , c - 1, dp);
-        int up = memoized(r - 1, c, dp);
-
-        dp[r][c] = left + up;
-
-        return dp[r][c];
+        return dp[m - 1][n - 1];
     }
-
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return memoized(m - 1, n - 1, dp);
+        return tabulation(m, n);
     }
 };
