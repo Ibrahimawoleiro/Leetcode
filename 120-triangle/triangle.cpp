ca70d15256e64eddd29735f1caba62bbf01e0399
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-08-21 21:37:25 -0400

    Time: 4 ms (73.56%) | Memory: 11.5 MB (62.18%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -33,38 +33,71 @@ public:
     //     return dp[r][c];
     // }
 
-    int tabulation(vector<vector<int>>& triangle){
+    // // TC -> O(N * M)
+    // // SC -> O(N * M)
+    // int tabulation(vector<vector<int>>& triangle){
+    //     int row_size = triangle.size();
+    //     int col_size = triangle[triangle.size() - 1].size();
+    //     vector<vector<int>> dp(row_size, vector<int>(col_size, INT_MAX));
+    //     int min_path;
+    //     for (int r = 0; r < row_size; r++){
+    //         min_path = INT_MAX;
+    //         for (int c = 0; c < col_size; c++){
+    //             if (r == 0 && c == 0){
+    //                 dp[r][c] = triangle[r][c];
+    //                 min_path = dp[r][c];
+    //                 continue;
+    //             }
+    //             int upper = INT_MAX;
+    //             int diagonal = INT_MAX;
+    //             if (c >= triangle[r].size()) break;
+    //             if (r - 1 >= 0){
+    //                 upper = dp[r - 1][c];
+    //                 if (c - 1 >= 0){
+    //                     diagonal = dp[r - 1][c - 1];
+    //                 }
+    //             }
+    //             int minimum = min(upper, diagonal);
+    //             dp[r][c] = triangle[r][c] + minimum;
+    //             (dp[r][c] < min_path)? min_path = dp[r][c]: min_path;
+    //         }
+    //     }
+    //     return min_path;
+    // }
+
+    int optimizedTabulation(vector<vector<int>>& triangle){
         int row_size = triangle.size();
         int col_size = triangle[triangle.size() - 1].size();
-        vector<vector<int>> dp(row_size, vector<int>(col_size, INT_MAX));
+        vector<vector<int>> prev(1, vector<int>(col_size, INT_MAX));
+        vector<vector<int>> curr(1, vector<int>(col_size, INT_MAX));
         int min_path;
         for (int r = 0; r < row_size; r++){
             min_path = INT_MAX;
             for (int c = 0; c < col_size; c++){
                 if (r == 0 && c == 0){
-                    dp[r][c] = triangle[r][c];
-                    min_path = dp[r][c];
+                    curr[r][c] = triangle[r][c];
+                    min_path = curr[0][0];
                     continue;
                 }
                 int upper = INT_MAX;
                 int diagonal = INT_MAX;
                 if (c >= triangle[r].size()) break;
                 if (r - 1 >= 0){
-                    upper = dp[r - 1][c];
+                    upper = prev[0][c];
                     if (c - 1 >= 0){
-                        diagonal = dp[r - 1][c - 1];
+                        diagonal = prev[0][c - 1];
                     }
                 }
                 int minimum = min(upper, diagonal);
-                dp[r][c] = triangle[r][c] + minimum;
-                (dp[r][c] < min_path)? min_path = dp[r][c]: min_path;
+                curr[0][c] = triangle[r][c] + minimum;
+                (curr[0][c] < min_path)? min_path = curr[0][c]: min_path;
             }
+            prev = curr;
         }
         return min_path;
     }
 
-
     int minimumTotal(vector<vector<int>>& triangle) {
-        return tabulation(triangle);
+        return optimizedTabulation(triangle);
     }
 };
