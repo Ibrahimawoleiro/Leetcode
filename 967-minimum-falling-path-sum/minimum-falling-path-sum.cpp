b25dc6de34c4b0f3df6e9ce7430564dc0dc16a69
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-08-21 23:07:31 -0400

    Time: 11 ms (70.70%) | Memory: 13.4 MB (78.19%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -40,17 +40,50 @@ public:
     //     return minimum;
     // }
 
-    int tabulation(vector<vector<int>>& matrix){
+    // //TC -> O(N * M * 3)
+    // //SC -> O(N * M)
+    // int tabulation(vector<vector<int>>& matrix){
+    //     int row_size = matrix.size();
+    //     int col_size = matrix[0].size();
+    //     vector<vector<int>> dp(row_size, vector<int>(col_size, -1));
+    //     int ans;
+    //     for (int r = row_size - 1; r >= 0; r--){
+    //         ans = INT_MAX;
+    //         for (int c = 0; c < col_size; c++){
+    //             if (r == row_size - 1){
+    //                 dp[r][c] = matrix[r][c];
+    //                 ans = min(ans, dp[r][c]);
+    //                 continue;
+    //             } 
+    //             int left = INT_MAX;
+    //             int right = INT_MAX;
+    //             int below = INT_MAX;
+    //             if (c - 1 >= 0){
+    //                 left = dp[r + 1][c - 1];
+    //             }
+    //             if (c + 1 < col_size){
+    //                 right = dp[r + 1][c + 1];
+    //             }
+    //             below = dp[r + 1][c];
+    //             dp[r][c] = matrix[r][c] + min(min(left, right), below);
+    //             ans = min(ans, dp[r][c]);
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+    int optimizedTabulation(vector<vector<int>>& matrix){
         int row_size = matrix.size();
         int col_size = matrix[0].size();
-        vector<vector<int>> dp(row_size, vector<int>(col_size, -1));
+        vector<int> prev(col_size, -1);
+        vector<int> curr(col_size, -1);
         int ans;
         for (int r = row_size - 1; r >= 0; r--){
             ans = INT_MAX;
             for (int c = 0; c < col_size; c++){
                 if (r == row_size - 1){
-                    dp[r][c] = matrix[r][c];
-                    ans = min(ans, dp[r][c]);
+                    curr[c] = matrix[r][c];
+                    ans = min(ans, curr[c]);
                     continue;
                 } 
 
@@ -59,19 +92,20 @@ public:
                 int below = INT_MAX;
 
                 if (c - 1 >= 0){
-                    left = dp[r + 1][c - 1];
+                    left = prev[c - 1];
                 }
                 if (c + 1 < col_size){
-                    right = dp[r + 1][c + 1];
+                    right = prev[c + 1];
                 }
-                below = dp[r + 1][c];
-                dp[r][c] = matrix[r][c] + min(min(left, right), below);
-                ans = min(ans, dp[r][c]);
+                below = prev[c];
+                curr[c] = matrix[r][c] + min(min(left, right), below);
+                ans = min(ans, curr[c]);
             }
+            prev = curr;
         }
         return ans;
     }
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        return tabulation(matrix);
+        return optimizedTabulation(matrix);
     }
 };
