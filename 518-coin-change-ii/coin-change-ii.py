e0eec481405caf321cd1a95550cf7599290260d3
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-08-24 13:52:57 -0400

    Time: 376 ms (32.50%) | Memory: 58.8 MB (23.99%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -18,23 +18,38 @@ class Solution:
 
         dp = [[-1 for val in range(amount + 1)] for num in range(len(coins))]
 
+        # def memoized(index, rem, dp):
+        #     if rem == 0:
+        #         return 1
+        #     if index == 0:
+        #         if rem % coins[index] == 0:
+        #             return 1
+        #         return 0
+        #     if (dp[index][rem] != -1):
+        #         return dp[index][rem]
+        #     not_take = memoized(index - 1, rem, dp)
+        #     take = 0
+        #     if rem >= coins[index]:
+        #         take = memoized(index, rem - coins[index], dp)
+        #     dp[index][rem] = take + not_take
+        #     return dp[index][rem]
 
-
-        def memoized(index, rem, dp):
-            if rem == 0:
-                return 1
-            if index == 0:
-                if rem % coins[index] == 0:
-                    return 1
-                return 0
-            if (dp[index][rem] != -1):
-                return dp[index][rem]
-            not_take = memoized(index - 1, rem, dp)
-            take = 0
-            if rem >= coins[index]:
-                take = memoized(index, rem - coins[index], dp)
-            dp[index][rem] = take + not_take
-            return dp[index][rem]
-
-        return memoized(len(coins) - 1, amount, dp)
+        def tabulation():
+            dp = [[-1 for _ in range(amount + 1)] for num in range(len(coins))]
+            for index in range(len(coins)):
+                for goal in range(amount + 1):
+                    if index == 0:
+                        if goal % coins[index] == 0:
+                            dp[index][goal] = 1
+                        else:
+                            dp[index][goal] = 0
+                    else:
+                        not_take = dp[index - 1][goal]
+                        take = 0
+                        if goal >= coins[index]:
+                            take = dp[index][goal - coins[index]]
+                        dp[index][goal] = take + not_take
+            print(dp)
+            return dp[len(dp) - 1][amount]
+        return tabulation()
 
