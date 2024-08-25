6e7a31829d71802362db19cac440648572a797b9
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-08-25 00:53:23 -0400

    Time: 19 ms (81.44%) | Memory: 26.1 MB (25.92%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -30,6 +30,8 @@ public:
     //     return dp[i][j];
     // }
 
+    // TC -> O(N * M * 2 * 2)
+    // SC -> O(N * M)
     int tabulation(int i, int j, string text1, string text2){
         vector<vector<int>> dp(text1.size()+1, vector<int>(text2.size()+1, -1));
         int text1_size = text1.size();
@@ -37,12 +39,9 @@ public:
         for(int i = 0; i <= text1_size; i++){
             dp[i][0] = 0;
         }
-
         for(int j = 0; j <= text2_size; j++){
             dp[0][j] = 0;
         }
-
-
         for (int i = 1; i <= text1_size; i++){
             for(int j = 1; j <= text2_size; j++){
                 if (text1[i - 1] == text2[j - 1]){
@@ -52,9 +51,9 @@ public:
                 }
             }
         }
-
         return dp[text1_size][text2_size];
     }
+
     int longestCommonSubsequence(string text1, string text2) {
         
         return tabulation(text1.size() - 1, text2.size() - 1, text1, text2);
