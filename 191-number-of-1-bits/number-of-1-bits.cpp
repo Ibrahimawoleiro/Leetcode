8ec775aacdf2522654afac47c837f9dc7e014993
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-08-26 14:23:20 -0400

    Time: 4 ms (30.91%) | Memory: 8 MB (28.32%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -3,7 +3,6 @@ public:
     int hammingWeight(int n) {
         int count = 0;
         for(int i = 0; i < 32; i ++){
-            cout << (n &&1 << i) << endl;
             if ((n & (1 << i)) != 0){
                 count += 1;
             }
