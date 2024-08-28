f8bfd975cf0bb057c68bacc67b27dda62b515e54
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-08-28 16:27:53 -0400

    Time: 50 ms (10.62%) | Memory: 31.1 MB (6.09%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -21,35 +21,61 @@ public:
     //     return max(buy, sell);
     // }
 
-    int memoized(vector<int>& prices,int index, int rem, bool bought, vector<vector<vector<int>>>& dp){
-        if (rem <= 0){
-            return 0;
-        }
-        if (index == prices.size() - 1){
-            if (bought){
-                return prices[index];
-            }else{
-                return 0;
+    // int memoized(vector<int>& prices,int index, int rem, bool bought, vector<vector<vector<int>>>& dp){
+    //     if (rem <= 0){
+    //         return 0;
+    //     }
+    //     if (index == prices.size() - 1){
+    //         if (bought){
+    //             return prices[index];
+    //         }else{
+    //             return 0;
+    //         }
+    //     }
+    //     int transaction = bought ? 1: 0;
+    //     // cout << index << " " << rem << " "<< transaction << endl;
+    //     if (dp[index][rem][transaction] != -1){
+    //         return dp[index][rem][transaction];
+    //     }
+    //     int buy = 0;
+    //     int sell = 0;
+    //     if(!bought){
+    //         buy = max(-prices[index] + memoized(prices, index + 1, rem, true, dp), memoized(prices, index + 1, rem, false, dp));
+    //     }else{
+    //         sell = max(prices[index] + memoized(prices, index + 1, rem - 1, false, dp), memoized(prices, index + 1, rem, true, dp));
+    //     }
+    //     dp[index][rem][transaction] = max(buy, sell);
+    //     return dp[index][rem][transaction];
+    // }
+
+    int tabulation(vector<int>& prices, int k){
+        vector<vector<vector<int>>> dp(prices.size(), vector<vector<int>>(k + 1, vector<int>(2, 0)));
+        for(int index = prices.size() - 1; index >= 0; index--){
+            for(int bought = 0; bought <= 1; bought++){
+                for(int rem = 0; rem <= k; rem ++){
+                    if (rem == 0){
+                        dp[index][rem][bought] = 0;
+                    }else if(index == prices.size() - 1){
+                        if (bought == 1){
+                            dp[index][rem][bought] = prices[index];
+                        }
+                    }else{
+                        int buy = 0;
+                        int sell = 0;
+                        if(bought == 0){
+                            buy = max(-prices[index] + dp[index + 1][rem][1], dp[index + 1][rem][0]);
+                        }else{
+                            sell = max(prices[index] + dp[index + 1][rem - 1][0],dp[index + 1][rem][1]);
+                        }
+                        dp[index][rem][bought] = max(buy, sell);
+                    }
+                }
             }
         }
-        int transaction = bought ? 1: 0;
-        // cout << index << " " << rem << " "<< transaction << endl;
-        if (dp[index][rem][transaction] != -1){
-            return dp[index][rem][transaction];
-        }
-        int buy = 0;
-        int sell = 0;
-        if(!bought){
-            buy = max(-prices[index] + memoized(prices, index + 1, rem, true, dp), memoized(prices, index + 1, rem, false, dp));
-        }else{
-            sell = max(prices[index] + memoized(prices, index + 1, rem - 1, false, dp), memoized(prices, index + 1, rem, true, dp));
-        }
-        dp[index][rem][transaction] = max(buy, sell);
-        return dp[index][rem][transaction];
+        return dp[0][k][0];
     }
 
     int maxProfit(int k, vector<int>& prices) {
-        vector<vector<vector<int>>> dp(prices.size(), vector<vector<int>>(k + 1, vector<int>(2, -1)));
-        return memoized(prices, 0, k, false, dp);
+        return tabulation(prices, k);
     }
 };
