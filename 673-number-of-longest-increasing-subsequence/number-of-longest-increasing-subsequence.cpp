885de9e62a074f54a3c1e0fe3aa206a9eed3219e
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-08-30 14:22:16 -0400

    Time: 37 ms (62.95%) | Memory: 16.1 MB (52.43%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    int tabulation(vector<int>& nums) {
-    if (nums.empty()) return 0;
-
-    vector<int> lis_count(nums.size(), 1);
-    vector<int> lis_length(nums.size(), 1);
-    
-    for (int index = 0; index < nums.size(); index++) {
-        for (int s = index - 1; s >= 0; s--) {
-            if (nums[s] < nums[index]) {
-                if (lis_length[s] + 1 > lis_length[index]) {
-                    lis_length[index] = lis_length[s] + 1;
-                    lis_count[index] = lis_count[s];
-                } else if (lis_length[s] + 1 == lis_length[index]) {
-                    lis_count[index] += lis_count[s];
+    int tabulation(vector<int>& nums){
+        vector<int> lis_count(nums.size(), 1);
+        vector<int> lis_length(nums.size(), 1);
+        for(int index = 0; index < nums.size(); index ++){
+            for(int s = index - 1; s >= 0; s--){
+                if(nums[s] < nums[index]){
+                    if(lis_length[s]+ 1 > lis_length[index]){
+                        lis_length[index] = lis_length[s] + 1;
+                        lis_count[index] = lis_count[s];
+                    }else if(lis_length[s]+ 1 == lis_length[index]){
+                        lis_count[index] += lis_count[s];
+                    }
                 }
             }
         }
-    }
-
-    int maximum = *max_element(lis_length.begin(), lis_length.end());
-    int ans = 0;
-    
-    for (int index = 0; index < nums.size(); index++) {
-        if (lis_length[index] == maximum) {
-            ans += lis_count[index];
+        int maximum = -1;
+        for(auto num: lis_length){
+            maximum = max(maximum, num);
         }
+        int ans = 0;
+        for(int index = 0; index < nums.size(); index++){
+            if(lis_length[index] == maximum){
+                ans += lis_count[index];
+            }
+        }
+        return ans;
     }
-    
-    return ans;
-}
     int findNumberOfLIS(vector<int>& nums) {
         return tabulation(nums);
     }
