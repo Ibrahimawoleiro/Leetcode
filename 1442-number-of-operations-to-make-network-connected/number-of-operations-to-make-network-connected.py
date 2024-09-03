3f8ffa1b60ffc26e8e230192389035aa92b326e9
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-03 16:11:54 -0400

    Time: 394 ms (44.65%) | Memory: 36.6 MB (73.08%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
@@ -1,3 +1,51 @@
+class Union_find:
+    
+    def __init__(self,V:int):
+        self.rank = [0 for _ in range(V)]
+        self.ultimate_parent = [val for val in range(V)]
+        self.size = [1 for _ in range(V)]
+    def find_ultimate_parent(self,vertex):
+        ulp_vertex = self.ultimate_parent[vertex]
+        curr = vertex
+        while ulp_vertex != vertex:
+            vertex = ulp_vertex
+            ulp_vertex = self.ultimate_parent[ulp_vertex]
+            
+        self.ultimate_parent[curr] = ulp_vertex
+        return ulp_vertex
+        
+    def find(self,v1:int , v2:int):
+        v1_ultimate_parent = self.find_ultimate_parent(v1)
+        v2_ultimate_parent = self.find_ultimate_parent(v2)
+        return v1_ultimate_parent == v2_ultimate_parent
+        
+    def union_by_rank(self,v1, v2):
+        v1_ultimate_parent = self.find_ultimate_parent(v1)
+        v2_ultimate_parent = self.find_ultimate_parent(v2)
+        if self.rank[v1_ultimate_parent] > self.rank[v2_ultimate_parent]:
+            self.ultimate_parent[v2_ultimate_parent] = v1_ultimate_parent
+        elif self.rank[v2_ultimate_parent] > self.rank[v1_ultimate_parent]:
+            self.ultimate_parent[v1_ultimate_parent] = v2_ultimate_parent
+        else:
+            self.rank[v1_ultimate_parent] += 1
+            self.ultimate_parent[v2_ultimate_parent] = v1_ultimate_parent
+            
+    def union_by_size(self,v1, v2):
+        v1_ultimate_parent = self.find_ultimate_parent(v1)
+        v2_ultimate_parent = self.find_ultimate_parent(v2)
+        if self.size[v1_ultimate_parent] > self.size[v2_ultimate_parent]:
+            self.ultimate_parent[v2_ultimate_parent] = v1_ultimate_parent
+            self.size[v1_ultimate_parent] += self.size[v2_ultimate_parent]
+        elif self.rank[v2_ultimate_parent] > self.rank[v1_ultimate_parent]:
+            self.ultimate_parent[v1_ultimate_parent] = v2_ultimate_parent
+            self.size[v2_ultimate_parent] += self.size[v1_ultimate_parent]
+        else:
+            self.size[v1_ultimate_parent] += self.size[v2_ultimate_parent]
+            self.ultimate_parent[v2_ultimate_parent] = v1_ultimate_parent
+    
+    def union(self,v1, v2):
+        self.union_by_rank(v1, v2)
+
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
         
@@ -7,7 +55,6 @@ class Solution:
         graph = [[] for num in range(n)]
         visited = [0 for num in range(n)]
         def dfs(curr):
-            print(curr,'iguhsjklvdcx')
             if visited[curr] == 1:
                 return 
             visited[curr] = 1
@@ -23,10 +70,8 @@ class Solution:
         
         for num in range(n):
             if visited[num] != 1:
-                print(num)
                 con_comp_count += 1
                 dfs(num)
-        print(graph)
         if len(connections) < n - 1:
             return -1
 
