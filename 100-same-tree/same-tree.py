7c19c6d830f6cb44f5e28c796c2becbe19a0ceba
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-04 12:57:08 -0400

    Time: 37 ms (43.16%) | Memory: 16.6 MB (38.33%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -6,32 +6,11 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        # #Approach 1
-        # arr1 = []
-        # arr2 = []
+        def re(n, m):
+            if not n and not m:
+                return True
+            if not n and m or n and not m or n.val != m.val:
+                return False
+            return re(n.left, m.left) and re(n.right, m.right)
 
-        # def helper(root,array,direction):
-
-        #     if not root:
-        #         return 
-                
-        #     array.append(str(root.val) + direction)
-
-        #     helper(root.left, array,'left') if root.left else array.append(None)
-               
-        #     helper(root.right, array,'right') if root.right else array.append(None)
-                
-        #     return array
-        
-        # return helper(p,arr1,'root') == helper(q,arr2,'root')
-
-        #Approach 2
-        if not p and not q:
-            return True
-        if not p and q or p and not q:
-            return False
-        
-        if p.val != q.val:
-            return False
-        
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right,q.right)
+        return re(p,q)
