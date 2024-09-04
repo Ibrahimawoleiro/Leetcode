6bc0123b81f5fb1edff64b27b0e3ffc1475aa720
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-03 22:13:12 -0400

    Time: 140 ms (46.74%) | Memory: 18.1 MB (19.86%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
@@ -1,54 +1,77 @@
+class Union_find:
+    
+    def __init__(self,V:int):
+        self.rank = [0 for _ in range(V)]
+        self.ultimate_parent = [val for val in range(V)]
+        self.size = [1 for _ in range(V)]
+    def find_ultimate_parent(self,vertex):
+        ulp_vertex = self.ultimate_parent[vertex]
+        parent = ulp_vertex
+        curr = vertex
+        while ulp_vertex != vertex:
+            vertex = ulp_vertex
+            ulp_vertex = self.ultimate_parent[ulp_vertex]
+        self.ultimate_parent[curr] = ulp_vertex
+        self.ultimate_parent[parent] = ulp_vertex
+        return ulp_vertex
+        
+    def find(self,v1:int , v2:int):
+        v1_ultimate_parent = self.find_ultimate_parent(v1)
+        v2_ultimate_parent = self.find_ultimate_parent(v2)
+        return v1_ultimate_parent == v2_ultimate_parent
+        
+    def union_by_rank(self,v1, v2):
+        v1_ultimate_parent = self.find_ultimate_parent(v1)
+        v2_ultimate_parent = self.find_ultimate_parent(v2)
+        if self.rank[v1_ultimate_parent] > self.rank[v2_ultimate_parent]:
+            self.ultimate_parent[v2_ultimate_parent] = v1_ultimate_parent
+        elif self.rank[v2_ultimate_parent] > self.rank[v1_ultimate_parent]:
+            self.ultimate_parent[v1_ultimate_parent] = v2_ultimate_parent
+        else:
+            self.rank[v1_ultimate_parent] += 1
+            self.ultimate_parent[v2_ultimate_parent] = v1_ultimate_parent
+            
+    def union_by_size(self,v1, v2):
+        v1_ultimate_parent = self.find_ultimate_parent(v1)
+        v2_ultimate_parent = self.find_ultimate_parent(v2)
+        if self.size[v1_ultimate_parent] > self.size[v2_ultimate_parent]:
+            self.ultimate_parent[v2_ultimate_parent] = v1_ultimate_parent
+            self.size[v1_ultimate_parent] += self.size[v2_ultimate_parent]
+        elif self.size[v2_ultimate_parent] > self.size[v1_ultimate_parent]:
+            self.ultimate_parent[v1_ultimate_parent] = v2_ultimate_parent
+            self.size[v2_ultimate_parent] += self.size[v1_ultimate_parent]
+        else:
+            self.size[v1_ultimate_parent] += self.size[v2_ultimate_parent]
+            self.ultimate_parent[v2_ultimate_parent] = v1_ultimate_parent
+    
+    def union(self,v1, v2):
+        self.union_by_size(v1, v2)
+
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
-        row_len = 0
-        col_len = 0
+        
+        row_size = 0
+        col_size = 0
         store = set()
         for location in stones:
-            row_len = max(row_len, location[0])
-            col_len = max(col_len, location[1])
-
-        row_len += 1
-        col_len += 1
-
-        parent = [num for num in range(row_len + col_len)]
-        size = [1 for num in range(row_len + col_len)]
-
-        def find_ul_parent(curr):
-            u_p = parent[parent[curr]]
-            c_p = parent[curr]
-            while c_p != u_p:
-                c_p = u_p
-                u_p = parent[u_p]
-            parent[curr] = u_p
-
-            return u_p
-
+            row_size = max(row_size,location[0])
+            col_size = max(col_size, location[1])
+        row_size += 1
+        col_size += 1
+        num_of_nodes = row_size + col_size
+        u_f = Union_find(num_of_nodes)
         for location in stones:
+            x = location[0]
+            y = location[1] + row_size 
+            u_f.union(x,y)
+            store.add(x)
+            store.add(y)
 
-            column = (row_len  - 1 ) + location[1] + 1
-            row = location[0]
-
-            store.add(row)
-            store.add(column)
-
-
-            column_ul = find_ul_parent(column)
-            row_ul = find_ul_parent(row)
-
-            if size[row_ul] == size[column_ul]:
-                size[row_ul] += size[column_ul]
-                parent[column_ul] = row_ul
-            else:
-                if size[row_ul] > size[column_ul]:
-                    size[row_ul] += size[column_ul]
-                    parent[column_ul] = row_ul
-                else:
-                    size[column_ul] += size[row_ul]
-                    parent[row_ul] = column_ul
-        
         count = 0
         for num in store:
-            if num == find_ul_parent(num):
+            if num == u_f.find_ultimate_parent(num):
                 count += 1
-
+        print(store)
         return len(stones) - count
+
+
