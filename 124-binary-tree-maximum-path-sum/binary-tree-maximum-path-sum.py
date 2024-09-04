e7fd4e606809dd5d92485862c73b25c425b9f333
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-04 11:54:18 -0400

    Time: 73 ms (12.99%) | Memory: 21.2 MB (8.92%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
@@ -10,22 +10,16 @@ class Solution:
         ans = [-float('inf')]
         self.helper(root, ans)
         return ans[0]
-      
-    
+
     def helper(self, node,ans):
         if not node:
             return 0
-
         l = self.helper(node.left,ans)
         r = self.helper(node.right,ans)
-
         curr = l + r + node.val
         curr_only = node.val
         l_and_curr = node.val + l
         r_and_curr = node.val + r
-        
         result = max(curr, max(max(curr_only, l_and_curr), r_and_curr))
         ans[0] = max(result, ans[0])
         return max(max(l_and_curr, r_and_curr),curr_only)
-        # heapq.heappush(heap, min(min(-curr, -node.val),-(node.val + max(l, r))))
-        # return max(node.val + max(l, r), node.val)
