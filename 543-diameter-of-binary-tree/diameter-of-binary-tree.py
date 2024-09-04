7eb84b50c38db8997ba551e8c836b217b683cba7
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-04 11:37:58 -0400

    Time: 39 ms (93.95%) | Memory: 19.2 MB (97.32%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -7,19 +7,15 @@
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
 
-        def helper(x):
-            if not x:
-                return (0,0)
+        def re(node, overall_ans):
+            if not node:
+                return 0
+            left = re(node.left, overall_ans)
+            right = re(node.right, overall_ans)   
+            if left + right > overall_ans[0]:
+                overall_ans[0] = left+right
+            return max(left, right) + 1    
 
-            l = (0,0)
-            r = (0,0)
-            if x.left:
-                l = helper(x.left)
-            if x.right:
-                r = helper(x.right)
-            print(l[0]+r[0], x.val)
-            return (1 + max(l[0],r[0]), l[0]+r[0] if (l[0]+r[0] > r[1] and l[0]+r[0] > l[1]) else max(r[1], l[1]))
-            
-        return helper(root)[1]
-            
-
+        ans = [0]
+        re(root, ans)
+        return ans[0]
