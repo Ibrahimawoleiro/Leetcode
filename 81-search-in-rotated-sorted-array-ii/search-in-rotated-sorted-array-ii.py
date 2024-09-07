e52179520983ea0690be3756c800d6eba8ad6cb8
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-07 18:48:10 -0400

    Time: 59 ms (14.32%) | Memory: 17.7 MB (29.72%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -1,40 +1,36 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-          #Binary Search
-        def bs(s, e):
-            while(s<= e):
-                mid = (s + e)// 2
+        def bs(s , e):
+            left = s
+            right = e
+            ans = False
+            while left <= right:
+                mid = (left + right) // 2
                 if nums[mid] == target:
                     return True
                 elif nums[mid] > target:
-                    e = mid - 1
+                    right = mid - 1
                 else:
-                    s = mid + 1
-            return False
-
+                    left = mid + 1
+            return ans
         def rec(s, e):
-            
-            if s > e:
-                return False
-            
-            mid = (s + e)//2
-
+            if s >= e:
+                if s == e:
+                    if nums[s] == target:
+                        return True
+                else:
+                    return False
+            mid = (s + e) // 2
             if nums[mid] == target:
                 return True
-            #If the target might be located in the sorted part
-            if nums[mid] < nums[e] and target <= nums[e] and target >= nums[mid]:
-                return bs(mid + 1, e)
             if nums[mid] > nums[s] and target >= nums[s] and target < nums[mid]:
-                return bs(s, mid - 1)
-            #If the target might be located in one of the unsorted part
-            left_unsorted = False
-            if nums[mid] <= nums[s] and (target < nums[mid] or target >= nums[s]):
-                left_unsorted =  rec(s, mid - 1)
-            if left_unsorted:
-                return True
-            
-            if nums[mid] >= nums[e] and (target <= nums[e] or target > nums[mid]):
-                return rec(mid + 1, e)
-
-            return False
-        return rec(0, len(nums) - 1)
+                return bs(s,mid - 1)
+            elif nums[mid] < nums[e] and target > nums[mid] and target <= nums[e]:
+                return bs(mid+ 1, e)
+            else:
+                left = rec(s, mid - 1)
+                right = rec(mid + 1, e)
+                if left != False:
+                    return left
+                return right
+        return rec(0,len(nums) - 1)
