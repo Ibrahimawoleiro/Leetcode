b2e4bd8d02799286db473d52ba7663aae5ae0b62
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-07 22:59:32 -0400

    Time: 1452 ms (5.04%) | Memory: 30.7 MB (6.68%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
@@ -1,50 +1,26 @@
 class Solution:
     def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
-        l = 0
-        h = len(bloomDay) - 1
-        print(min(bloomDay))
-        print(max(bloomDay))
-        days = bloomDay.copy()
-        days.sort()
-        
-        ans = float('inf')
-
-        #Use a binary search through the days options
-        while l <= h:
-            #Get the middle day 
-            mid = (l + h) // 2
-            curr = days[mid]
-
+        low = 1
+        high = max(bloomDay)
+        ans = -1
+        while low <= high:
+            mid = (low + high) // 2
+            bouquet_count = 0
             i = 0
-            j = k - 1
-
-            #Initialize a counter for the number of m length you can get from taking days[mid]
-            count = 0
-            #Check if you have enough k flowers which are less than or equal days[mid]
-            while(j < len(bloomDay)):
-                a = i
-                while(a <= j):
-                    if bloomDay[a] > curr:
+            while i < len(bloomDay):
+                flower_count = 0
+                for j in range(k):
+                    if i + j >= len(bloomDay) or bloomDay[i + j] > mid:
+                        i = i + j + 1
                         break
-                    a += 1
-                if a <= j:
-                    i = a + 1
-                    j = i + k - 1
-                    continue
-                if a == j + 1:
-                    count += 1
+                    flower_count += 1
+                if flower_count == k:
+                    i = i + k
+                    bouquet_count += 1
                 
-                i = j + 1
-                j = j + k
-
-            if count >= m:
-                if days[mid] < ans:
-                    ans = days[mid]
-                h = mid - 1
+            if bouquet_count >= m:
+                ans = mid
+                high = mid - 1
             else:
-                l = mid + 1
-
-        return ans if ans < float('inf') else -1
-
-
-        
+                low = mid + 1
+        return ans
