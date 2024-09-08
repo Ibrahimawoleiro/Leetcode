2337060c0b36f9f09432c8193809ae051c4c16d1
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-08 00:01:05 -0400

    Time: 604 ms (6.30%) | Memory: 22.4 MB (25.25%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -1,34 +1,27 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-        l = 1
-        h = sum(weights)
-        ans = float('inf')
-        while l <= h:
-            mid = (l + h) // 2
-            total = 0
-            count = 0
-            ReachEnd = True
-            for i in range(len(weights)):
-                if weights[i] > mid:
-                    ReachEnd = False
-                    break
-                total += weights[i]
-                if total >= mid:
-                    if total > mid:
-                        total = weights[i]
-                    else:
-                        total = 0
-                    count += 1
-                if total > 0 and i == len(weights) - 1:
-                    count += 1
-            if not ReachEnd:
-                l = mid + 1
-                continue
-            if count <= days:
-                if mid < ans:
-                    ans = mid
-                h = mid - 1
-            else:
-                l = mid + 1
+        low = max(weights)
+        high = sum(weights)
+        ans = 0
 
+        while low <= high:
+            capacity = (low + high) // 2
+            days_taken = 0
+            total = 0
+            for index in range(len(weights)):
+                if total + weights[index] <= capacity:
+                    total += weights[index]
+                    if index == len(weights) - 1:
+                        days_taken += 1
+                else:
+                    days_taken += 1
+                    total = weights[index]
+                    if index == len(weights) - 1:
+                        days_taken += 1
+
+            if days_taken <= days:
+                ans = capacity
+                high = capacity - 1
+            else:
+                low = capacity + 1
         return ans
