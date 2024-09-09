61f59603cee823f990144b0d91ee04bff409d2fb
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-09 00:14:04 -0400

    Time: 154 ms (5.44%) | Memory: 18.9 MB (6.79%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -0,0 +1,42 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        max_right = [0 for _ in range(len(height))]
+        max_left = [0 for _ in range(len(height))]
+        right_stack = []
+        left_stack = []
+        total = 0
+        def find_right_boundary(index):
+            result = height[index]
+            while right_stack and height[index] >= right_stack[-1]:
+                right_stack.pop()
+            if right_stack:
+                result = right_stack[-1]
+            else:
+                right_stack.append(height[index])
+            return result
+
+        def find_left_boundary(index):
+            result = height[index]
+            while left_stack and height[index] >= left_stack[-1]:
+                left_stack.pop()
+            if left_stack:
+                result = left_stack[-1]  
+            else:
+                left_stack.append(height[index])
+            return result
+        
+        for index in range(len(height)):
+            curr = find_left_boundary(index)
+            max_left[index] = curr
+
+        for index in range(len(height) - 1, -1, -1):
+            curr = find_right_boundary(index)
+            max_right[index] = curr
+
+        for index in range(len(height)):
+            water_at_curr = min(max_left[index], max_right[index]) - height[index]
+            total += water_at_curr
+        print(max_left)
+        print(height)
+        print(max_right)
+        return total
