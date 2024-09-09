1178b8c36f64f14ed80b733e0cd33b754de87c83
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-09 14:34:45 -0400

    Time: 86 ms (34.57%) | Memory: 17.7 MB (80.45%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,36 +1,34 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        stack = []
-
-        for val in asteroids:
-            if not stack:
-                stack.append(val)
-            else:
-                if stack[-1] < 0:
-                    stack.append(val)
+        state = []
+        """
+        State Incoming
+        ->      ->
+        ->      <- Collision
+        <-      <-
+        <-      ->
+        """
+        for asteroid in asteroids:
+            if state:
+                #If the incoming asteroid is moving left
+                if asteroid < 0:
+                    if state[-1] < 0:
+                        state.append(asteroid)
+                    else:
+                        while state[-1] > 0:
+                            if state[-1] > abs(asteroid):
+                                break
+                            elif state[-1] == abs(asteroid):
+                                state.pop()
+                                break
+                            else:
+                                state.pop()
+                                if not state or state[-1] < 0:
+                                    state.append(asteroid)
+                                    break
+                #If the incoming asteroid is moving right
                 else:
-                    if stack[-1] > 0:
-                        if val > 0:
-                            stack.append(val)
-                        elif val < 0:
-                            curr = abs(val)
-                            curr_win = True
-                            while curr_win:
-                                if len(stack) > 0:
-                                    back_of_stack = stack[-1]
-                                    if back_of_stack < 0:
-                                        stack.append(val)
-                                        break
-                                if len(stack) > 0 and curr > back_of_stack:
-                                    stack.pop()
-                                elif len(stack) == 0:
-                                    stack.append(val)
-                                    curr_win = False
-                                else:
-                                    if curr == stack[-1]:
-                                        stack.pop()
-                                        curr_win = False
-                                    else:
-                                        curr_win = False
-
-        return stack
+                        state.append(asteroid)
+            else:
+                state.append(asteroid)
+        return state
