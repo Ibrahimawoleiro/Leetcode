ac50e84601e3ab7a083e818460b4e6bd456e012c
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-10 16:41:28 -0400

    Time: 354 ms (12.42%) | Memory: 17 MB (67.52%) - LeetSync

---
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        store = {'a':-1,
+                 'b':-1,
+                 'c':-1
+                 }
+        ans = 0
+        for index in range(len(s)):
+            store[s[index]] = index
+            if store['a'] >= 0 and store['b'] >= 0 and store['c'] >= 0:
+                print(store)
+                min_start = min(store['a'], store['b'], store['c'])
+                ans += min_start + 1
+
+        return ans
