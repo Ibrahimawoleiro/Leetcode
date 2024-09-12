ad4507e202d46d7c7a846d6cdaf15d05cd940dbe
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-12 11:17:31 -0400

    Time: 142 ms (41.17%) | Memory: 26.7 MB (10.98%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,32 +1,20 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        
-        def recur(s, e):
-
-            if s == e:
-                return nums[s]
-                
-            m = (s + e) // 2
-
-            mid_num = nums[m]
-
-            #Check the middle number to see if it is the unique number
-            if nums[m - 1] != mid_num and nums[m + 1] != mid_num:
-                return mid_num
-
-            #If we get here, we need to know if we
-            #are on the left or right side of the unique number
-            start = -1
-            if nums[m - 1] == mid_num:
-                start = m - 1
-            elif nums[m + 1] == mid_num:
-                start = m
-            
-            #We have gotten the index of starting pair
-            #Now we check if we are on left or right
-            if start % 2 == 0:
-                return recur(start + 2, e)
+        n = len(nums)
+        left = 0
+        right = n - 1
+        if n == 1:
+            return nums[0]
+        if nums[0] != nums[1]:
+            return nums[0]
+        if nums[right] != nums[right - 1]:
+            return nums[right]
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] != nums[mid - 1] and nums[mid] != nums[mid + 1]:
+                return nums[mid] 
+            elif (nums[mid] == nums[mid + 1] and mid % 2 == 0) or (nums[mid] == nums[mid - 1] and mid % 2 == 1): 
+                left = mid + 1
             else:
-                return recur(s, start)
-
-        return recur(0,len(nums) - 1)
+                right = mid - 1
+        return -1
