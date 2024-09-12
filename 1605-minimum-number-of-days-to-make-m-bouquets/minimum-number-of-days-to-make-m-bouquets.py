624bbfc3d93a39b43f694dadc4c0d628272cfe50
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-12 18:14:32 -0400

    Time: 782 ms (72.30%) | Memory: 30.7 MB (6.54%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
@@ -1,25 +1,45 @@
 class Solution:
     def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
-        low = 1
-        high = max(bloomDay)
-        ans = -1
-        while low <= high:
-            mid = (low + high) // 2
-            bouquet_count = 0
-            i = 0
-            while i < len(bloomDay):
-                flower_count = 0
-                for j in range(k):
-                    if i + j >= len(bloomDay) or bloomDay[i + j] > mid:
-                        i = i + j + 1
-                        break
-                    flower_count += 1
-                if flower_count == k:
-                    i = i + k
-                    bouquet_count += 1
-            if bouquet_count >= m:
-                ans = mid
-                high = mid - 1
-            else:
-                low = mid + 1
-        return ans
+        
+        def brute_force():
+            low = 1
+            high = max(bloomDay)
+            for days in (range(1, high + 1)):
+                bouquet = 0
+                flowers = 0
+                for flower_index in range(len(bloomDay)):
+                    if bloomDay[flower_index] <= days:
+                        flowers += 1
+                        if flowers == k:
+                            bouquet += 1
+                            flowers = 0
+                    else:
+                        flowers = 0
+                if bouquet == m:
+                    return days
+            return -1
+        def optimized():
+            low = 1
+            high = max(bloomDay)
+            ans = -1
+            while low <= high:
+                mid = (low + high) // 2
+                bouquet = 0
+                flowers = 0
+                for flower_bloom in bloomDay:
+                    if  flower_bloom <= mid:
+                        flowers += 1
+                        if flowers == k:
+                            bouquet += 1
+                            flowers = 0
+                    else:
+                        flowers = 0
+                if bouquet >= m:
+                    ans = mid
+                    high = mid - 1
+                elif bouquet < m:
+                    low = mid + 1
+                else:
+                    high =  mid - 1 
+            return ans
+        return optimized()
