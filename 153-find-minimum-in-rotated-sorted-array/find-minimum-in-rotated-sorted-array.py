85a45c5b3507d837df66b16b7559ad637c87c8e7
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-12 01:00:59 -0400

    Time: 39 ms (78.87%) | Memory: 17 MB (6.94%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,18 +1,21 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-
-           #Identify the minimum side and return the minimum
-           #Identify the unsorted side and call the recursive case on it
-        def rec(s, e):
-            if s > e:
+        #Identify the minimum side and return the minimum
+        #Identify the unsorted side and call the recursive case on it
+        if len(nums) == 1:
+            return nums[0]
+        n = len(nums)
+        def rec(start, end):
+            if start >= end:
+                if start == end:
+                    return nums[start]
                 return 10000
-            if s == e:
-                return nums[s]
-            mid = (s + e) // 2
-            if nums[mid] > nums[s]:
-                return min(nums[s], rec(mid + 1, e))
-            elif nums[mid] < nums[e]:
-                return min(nums[mid], rec(s, mid - 1))
+            mid = (start + end) // 2
+            if nums[mid] > nums[start]:
+                return min(nums[start], rec(mid + 1, end))
+            elif nums[mid] < nums[end]:
+                return min(nums[mid], rec(start , mid - 1))
             else:
-                return min(rec(s, mid), rec(mid+ 1, e))
-        return rec(0, len(nums) - 1)
+                return min(rec(start, mid - 1), rec(mid + 1 , end))
+        result = rec(0 , n - 1)
+        return result 
