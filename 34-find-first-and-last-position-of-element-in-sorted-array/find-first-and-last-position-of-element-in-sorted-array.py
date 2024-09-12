a1e9d7470f0a125fb280922ab3b9a6a8e3254deb
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-11 22:28:42 -0400

    Time: 84 ms (8.62%) | Memory: 17.8 MB (94.08%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,60 +1,32 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if len(nums) == 0:
-            return [-1, -1]
-
-        def lower(start , end, pos):
-            while(start <= end):
-                #Looking if the number exist in the array
-                if pos == -1:
-                    mid = (start + end) // 2
-
-                    if nums[mid] == target:
-                        pos = mid
-                        end = mid - 1
-                    elif nums[mid] < target:
-                        start = mid + 1
-                    else:
-                        end = mid - 1
-                #Looking for the starting point
+        n = len(nums) - 1
+        def find_first_occurrence():
+            ans = -1
+            left = 0
+            right = n
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] == target:
+                    ans = mid
+                    right = mid - 1
+                elif nums[mid] < target:
+                    left = mid + 1
                 else:
-                    mid = (start + end) // 2
-                    if nums[mid] == target:
-                        pos = mid
-                        end = mid - 1
-                    elif nums[mid] < target:
-                        start = mid + 1
-                    else:
-                        end = mid - 1
-            return pos
-        def upper(start, end , pos):
-            while(start <= end):
-                #Looking if the number exist in the array
-                if pos == -1:
-                    mid = (start + end) // 2
-
-                    if nums[mid] == target:
-                        pos = mid
-                        start = mid + 1
-                    elif nums[mid] < target:
-                        start = mid + 1
-                    else:
-                        end = mid - 1
-                #Looking for the ending point
+                    right = mid - 1
+            return ans 
+        def find_last_occurrence():
+            ans = -1
+            left = 0
+            right = n
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] == target:
+                    ans = mid
+                    left = mid + 1
+                elif nums[mid] < target:
+                    left = mid + 1
                 else:
-                    mid = (start + end) // 2
-                    if nums[mid] == target:
-                        pos = mid
-                        start = mid + 1
-                    elif nums[mid] < target:
-                        start = mid + 1
-                    else:
-                        end = mid - 1
-            return pos
-        start = lower(0,len(nums) - 1, -1)
-        if start == -1:
-            return [-1, -1]
-        end = upper(0, len(nums) - 1, -1)
-        return [start, end]
-
-
+                    right = mid - 1
+            return ans
+        return [find_first_occurrence(), find_last_occurrence()]
