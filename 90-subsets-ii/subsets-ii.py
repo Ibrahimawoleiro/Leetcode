5ca947772a26fc6248380ddcfb71984a4a0a0dc9
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-14 19:41:24 -0400

    Time: 30 ms (95.91%) | Memory: 16.7 MB (46.49%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,16 +1,16 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        ans = set()
-        ans.add(())
-        def helper(curr, i):
-            if i >= len(nums):
-                return 
-            for index in range(i ,len(nums)):
-                curr.append(nums[index])
-                ans.add(tuple(curr.copy()))
-                helper(curr, index + 1)
-                curr.pop()
-
-        helper([], 0)
-        return list(ans)
+        ans = [[]]
+        def rec(arr, combo,ans, i):
+            if i >= len(arr):
+                return
+            for index in range(i , len(arr)):
+                if index > i and arr[index] == arr[index - 1]:
+                    continue
+                combo.append(arr[index])
+                ans.append(combo.copy())
+                rec(arr, combo,ans, index + 1)
+                combo.pop()
+        rec(nums, [], ans, 0)
+        return ans
