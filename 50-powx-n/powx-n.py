2defe891a406a75d8dfd8a768238fc89e7d7ed23
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-14 01:33:46 -0400

    Time: 25 ms (97.33%) | Memory: 16.5 MB (52.12%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,23 +1,15 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n == 0:
-            return 1.0
-        
-        # If n is negative, convert it to positive and set a flag to invert the result later
         inverse = n < 0
         base = x
         exponent = abs(n)
         ans = 1.0
-        
-        # Loop to perform fast exponentiation
+        #We keep on looping as long as exponent > 0
         while exponent > 0:
-            # If the exponent is odd, multiply the current base with the result
-            if exponent % 2 == 1:
+            if exponent % 2 == 0:
+                base *= base 
+                exponent /= 2
+            else:
                 ans *= base
-            
-            # Square the base and halve the exponent
-            base *= base
-            exponent //= 2
-        
-        # If the original exponent was negative, invert the result
+                exponent -= 1
         return 1 / ans if inverse else ans
