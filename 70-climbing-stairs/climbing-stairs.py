4c702c7bd2b3fdb96ae885890975347de28b752f
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-15 18:56:29 -0400

    Time: 28 ms (91.17%) | Memory: 16.5 MB (61.66%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -3,7 +3,6 @@ class Solution:
         if n == 1 or n == 0:
             return 1
         return self.recursive(n - 1) + self.recursive(n - 2)
-    
     def memoized(self, n, dp):
         if n == 1 or n == 0:
             return 1
@@ -11,7 +10,6 @@ class Solution:
             return dp[n]
         dp[n] = self.memoized(n - 1, dp) + self.memoized(n - 2, dp)
         return dp[n]
-    
     def tabulation(self,n):
         dp = [-1 for _ in range(n + 1)]
         for i in range(n + 1):
@@ -20,6 +18,12 @@ class Solution:
             else:
                 dp[i] = dp[i - 1] + dp[i - 2]
         return dp[n]
-
+    def optimized_tabulation(self, n):
+        prev = curr = 1 
+        for index in range(2, n + 1):
+            temp = curr + prev
+            prev = curr
+            curr = temp
+        return curr
     def climbStairs(self, n: int) -> int:
-        return self.tabulation(n)
+        return self.optimized_tabulation(n)
