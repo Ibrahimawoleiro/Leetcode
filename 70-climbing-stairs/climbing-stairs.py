97d325b32f5c30365b582d80c6dcb75d84c553bb
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-15 18:41:44 -0400

    Time: 37 ms (38.03%) | Memory: 16.5 MB (61.66%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,45 +1,17 @@
 class Solution:
+    def recursive(self,n):
+        if n == 1 or n == 0:
+            return 1
+        return self.recursive(n - 1) + self.recursive(n - 2)
+    
+    def memoized(self, n, dp):
+        if n == 1 or n == 0:
+            return 1
+        if dp[n] != -1:
+            return dp[n]
+        dp[n] = self.memoized(n - 1, dp) + self.memoized(n - 2, dp)
+        return dp[n]
+
     def climbStairs(self, n: int) -> int:
-        # #Approach 1
-        # dictionary = {
-        #     0:1,
-        #     1:1
-        # }
-
-        # for val in range(n+1):
-        #     if val in dictionary:
-        #         continue
-        #     dictionary[val] = dictionary[val-1] + dictionary[val-2]
-        
-        # return dictionary[n] if n in dictionary else 0
-
-
-        # #Approach 2
-        # arr = [0 for val in range(n+1)]
-        # arr[0] = 1
-        # arr[1] = 1
-
-        # for val in range(n+1):
-        #     if val == 0 or val == 1:
-        #         continue
-            
-        #     arr[val] = arr[val - 1] + arr[val - 2]
-
-        # return arr[n]
-
-        #Approach 3
-        current = 1
-        previous = 1
-        
-        for val in range(n+1):
-            if val == 0 or val == 1:
-                continue
-            
-            temp = current
-            current = previous + current
-            previous = temp
-
-        return current
-
-
-        
+        dp = [-1 for _ in range(n + 1)]
+        return self.memoized(n, dp)
