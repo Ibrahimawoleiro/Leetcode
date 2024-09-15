effd63f7babd70c7c5b8eb01b462a20c1704cd00
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-14 20:07:13 -0400

    Time: 32 ms (78.72%) | Memory: 16.4 MB (74.60%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.py
+++ 216-combination-sum-iii/combination-sum-iii.py
@@ -1,19 +1,20 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        arr = [i + 1 for i in range(9)]
+        # K is the size of each subarray
+        # N is the target sum
+        nums = [number for number in range(1, 10)]
         ans = []
-        def helper(i,curr,s):
-            if len(curr) == k or s >= n:
-                if len(curr) == k and s == n:
-                    ans.append(curr.copy())
+        def rec(combo, rem, i):
+            if rem == 0 or i >= len(nums):
+                if rem == 0 and len(combo) == k:
+                    ans.append(combo.copy())
                 return
-            
-            for index in range(i , len(arr)):
-                c_sum = s + arr[index]
-                curr.append(arr[index])
-                helper(index + 1, curr, c_sum)
-                c_sum -= arr[index]
-                curr.pop()
-
-        helper(0, [], 0)
+            #take 
+            if rem >= nums[i]:
+                combo.append(nums[i])
+                rec(combo, rem - nums[i], i + 1)
+                combo.pop()
+            #Don't take
+            rec(combo, rem , i + 1)
+        rec([], n, 0)
         return ans
