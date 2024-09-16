d0def64196f233a7c084699064a6be950ff6b9ae
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-16 00:15:26 -0400

    Time: 34 ms (67.95%) | Memory: 16.5 MB (76.49%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,22 +1,22 @@
 class Solution:
-    def recursive(self,r, c, m, n):
-        if r < 0 or c < 0:
-            return 0
-        if r == 0 and c == 0:
-            return 1
-        else:
-            return self.recursive(r - 1, c, m , n) + self.recursive(r, c - 1, m, n)
+    # def recursive(self,r, c, m, n):
+    #     if r < 0 or c < 0:
+    #         return 0
+    #     if r == 0 and c == 0:
+    #         return 1
+    #     else:
+    #         return self.recursive(r - 1, c, m , n) + self.recursive(r, c - 1, m, n)
 
-    def memoized(self,r, c, m, n, dp):
-        if r < 0 or c < 0:
-            return 0
-        if r == 0 and c == 0:
-            return 1
-        if dp[r][c] != -1:
-            return dp[r][c]
-        else:
-            dp[r][c] = self.memoized(r - 1, c, m , n, dp) + self.memoized(r, c - 1, m, n, dp)
-        return dp[r][c]
+    # def memoized(self,r, c, m, n, dp):
+    #     if r < 0 or c < 0:
+    #         return 0
+    #     if r == 0 and c == 0:
+    #         return 1
+    #     if dp[r][c] != -1:
+    #         return dp[r][c]
+    #     else:
+    #         dp[r][c] = self.memoized(r - 1, c, m , n, dp) + self.memoized(r, c - 1, m, n, dp)
+    #     return dp[r][c]
     
     def tabulation(self,m,n):
         dp = [[-1 for _ in range(n)] for _ in range(m)]
@@ -36,6 +36,6 @@ class Solution:
     
     def optimized_tabulation(self, m,n):
         pass
-        
+
     def uniquePaths(self, m: int, n: int) -> int:
         return self.tabulation(m, n)
