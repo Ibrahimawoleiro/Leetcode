e30a050cf156a7f56711e41a3c4deb6dd0f8b15d
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-16 01:01:58 -0400

    Time: 120 ms (31.81%) | Memory: 18.5 MB (36.18%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -17,7 +17,15 @@ class Solution:
         #Recursive
         dp[r][c] = grid[r][c] + min(self.memoized(grid, r - 1,c,dp), self.memoized(grid, r, c - 1,dp))
         return dp[r][c]
-
+    
+    def tabulation(self, grid):
+        dp = [[0 for _ in range(len(grid[0]))] for _ in range(len(grid))]
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if r == 0 and c == 0:
+                    dp[r][c] = grid[0][0]
+                else:
+                    dp[r][c] = grid[r][c] + min(dp[r - 1][c] if r > 0 else float('inf'), dp[r][c - 1] if c > 0 else float('inf'))
+        return dp[len(grid) - 1][len(grid[0]) - 1]
     def minPathSum(self, grid: List[List[int]]) -> int:
-        dp = [[-1 for _ in range(len(grid[0]))] for _ in range(len(grid))]
-        return self.memoized(grid, len(grid) - 1, len(grid[0]) - 1, dp)
+        return self.tabulation(grid)
