1ea829bb953caf7592b4683f77abf72d6d2f9d1d
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-16 00:14:42 -0400

    Time: 46 ms (6.41%) | Memory: 16.6 MB (39.26%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,13 +1,14 @@
 class Solution:
     def recursive(self,r, c, m, n):
-        if r < 0 or c < 0 or r >= m or c >= n:
+        if r < 0 or c < 0:
             return 0
         if r == 0 and c == 0:
             return 1
         else:
             return self.recursive(r - 1, c, m , n) + self.recursive(r, c - 1, m, n)
+
     def memoized(self,r, c, m, n, dp):
-        if r < 0 or c < 0 or r >= m or c >= n:
+        if r < 0 or c < 0:
             return 0
         if r == 0 and c == 0:
             return 1
@@ -16,6 +17,22 @@ class Solution:
         else:
             dp[r][c] = self.memoized(r - 1, c, m , n, dp) + self.memoized(r, c - 1, m, n, dp)
         return dp[r][c]
-    def uniquePaths(self, m: int, n: int) -> int:
+    
+    def tabulation(self,m,n):
         dp = [[-1 for _ in range(n)] for _ in range(m)]
-        return self.memoized(m - 1, n - 1, m, n, dp)
+        for row in range(m):
+            for col in range(n):
+                if row == 0 and col == 0:
+                    dp[row][col] = 1
+                else:
+                    up = 0
+                    left = 0
+                    if row - 1 >= 0:
+                        up = dp[row - 1][col]
+                    if col - 1 >= 0:
+                        left = dp[row][col - 1]
+                    dp[row][col] = up + left
+        return dp[m - 1][n - 1]
+
+    def uniquePaths(self, m: int, n: int) -> int:
+        return self.tabulation(m, n)
