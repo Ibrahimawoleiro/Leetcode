e030f049c458ee7df6067bbf91fd878242250f26
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-16 01:04:11 -0400

    Time: 114 ms (41.37%) | Memory: 17.4 MB (92.97%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -27,5 +27,19 @@ class Solution:
                 else:
                     dp[r][c] = grid[r][c] + min(dp[r - 1][c] if r > 0 else float('inf'), dp[r][c - 1] if c > 0 else float('inf'))
         return dp[len(grid) - 1][len(grid[0]) - 1]
+    
+    def optimized_tabulation(self, grid):
+        prev = [0 for _ in range(len(grid[0]))]
+        curr = [0 for _ in range(len(grid[0]))]
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if r == 0 and c == 0:
+                    curr[c] = grid[0][0]
+                else:
+                    curr[c] = grid[r][c] + min(prev[c] if r > 0 else float('inf'), curr[c - 1] if c > 0 else float('inf'))
+            prev = curr
+            curr = [0 for _ in range(len(grid[0]))]
+        return prev[len(grid[0]) - 1]
+    
     def minPathSum(self, grid: List[List[int]]) -> int:
-        return self.tabulation(grid)
+        return self.optimized_tabulation(grid)
