8bff78a9c7edce00b4c66555eaa713ac752059b2
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-16 15:02:55 -0400

    Time: 142 ms (33.60%) | Memory: 17.3 MB (70.95%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -1,21 +1,54 @@
 class Solution:
+    def recursive(self, matrix, r, c):
+        if c < 0 or c >= len(matrix[0]):
+            return float('inf')
+        if r == 0:
+            return matrix[r][c]
+        #Recursive
+        left = float('inf')
+        middle = float('inf')
+        right = float('inf')
+        left = self.recursive(matrix, r - 1, c - 1)
+        middle = self.recursive(matrix, r - 1, c)
+        right = self.recursive(matrix, r - 1, c + 1)
+        result = matrix[r][c] + min(left, middle, right)
+        return result
+
+    def memoized(self, matrix, r, c, dp):
+        if c < 0 or c >= len(matrix[0]):
+            return float('inf')
+        if r == 0:
+            return matrix[r][c]
+        if dp[r][c] != -1:
+            return dp[r][c]
+        #Recursive
+        left = self.memoized(matrix, r - 1, c - 1, dp)
+        middle = self.memoized(matrix, r - 1, c, dp)
+        right = self.memoized(matrix, r - 1, c + 1, dp)
+        result = matrix[r][c] + min(left, middle, right)
+        dp[r][c] =  result
+        return result
+    
+    def tabulation(self, matrix):
+        dp = [[-1 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                if r == 0:
+                    dp[r][c] = matrix[r][c]
+                else:
+                    left = float('inf')
+                    middle = float('inf')
+                    right = float('inf')
+                    if c - 1 >= 0:
+                        left = matrix[r][c] + dp[r - 1][c - 1]
+                    middle = matrix[r][c] + dp[r - 1][c]
+                    if c + 1 < len(matrix[0]):
+                        right = matrix[r][c] + dp[r - 1][c + 1]
+                    dp[r][c] = min(left, middle, right)
+        ans = float('inf')
+        for val in dp[-1]:
+            ans = min(ans , val)
+        return ans
+
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        store = {}
-        min_sum = float('inf')
-
-        def helper(c,store):
-            if c[0] >= len(matrix) or c[0] < 0 or c[1] >= len(matrix[0]) or c[1] < 0:
-                return float('inf')
-            if c in store:
-                return store[c]
-            if c[0] == len(matrix) - 1:
-                store[c] = matrix[c[0]][c[1]]
-                return matrix[c[0]][c[1]]
-            curr = matrix[c[0]][c[1]] + min(min(helper((c[0] + 1, c[1] + 1), store), helper((c[0] + 1, c[1]),store))         , helper((c[0] + 1, c[1] - 1), store) )
-            store[c] = curr
-            return curr
-
-        for c_i in range(len(matrix[0])):
-            curr = helper((0, c_i), store)
-            min_sum = min(curr, min_sum)
-        return min_sum
+        return self.tabulation(matrix)
