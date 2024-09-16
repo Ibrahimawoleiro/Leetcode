7030b30e457f964d983f99d0af16a71fd39a485f
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-09-16 14:29:19 -0400

    Time: 62 ms (55.74%) | Memory: 17.9 MB (41.57%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,19 +1,32 @@
 class Solution:
-    def minimumTotal(self, triangle: List[List[int]]) -> int:
-        store = {}
-        def helper(r,i, store):
-            if r >= len(triangle):
-                return 0
-            if (r, i) in store:
-                return store[(r, i)]
-            curr = triangle[r][i]
-            min_path = curr + min(helper(r+1, i,store), helper(r+1, i+1,store))
-            store[(r, i)] = min_path
-            return store[(r, i)]
-        ans = helper(0, 0, store)
-        return ans
+    def recursive(self, triangle, r, c):
+        if c < 0 or c > r:
+            return float('inf')
+        if r == 0 and c == 0:
+            return triangle[r][c]
+        #Recursive
+        left_diagonal = self.recursive(triangle, r - 1, c - 1)
+        right_diagonal = self.recursive(triangle, r - 1, c)
+        result = triangle[r][c] + min(left_diagonal, right_diagonal)
+        return result
 
-        # [-1],
-        # [2,3],
-        #[1,-1,-3]
-                    
+    def memoization(self, triangle, r, c, dp):
+        if c < 0 or c > r:
+            return float('inf')
+        if r == 0 and c == 0:
+            return triangle[r][c]
+        if dp[r][c] != -1:
+            return dp[r][c]
+        #Recursive
+        left_diagonal = self.memoization(triangle, r - 1, c - 1, dp)
+        right_diagonal = self.memoization(triangle, r - 1, c, dp)
+        result = triangle[r][c] + min(left_diagonal, right_diagonal)
+        dp[r][c] = result
+        return result
+        
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        min_path = float('inf')
+        dp = [[-1 for _ in range(len(triangle[index]))] for index in range(len(triangle))]
+        for c in range(len(triangle[-1])):
+            min_path = min(min_path, self.memoization(triangle, len(triangle) - 1, c, dp))
+        return min_path
