cc656344f4fa531f25960474475c976d2c52a31f
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-10-11 12:03:24 -0400

    Time: 33 ms (75.82%) | Memory: 16.5 MB (83.78%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        negative = x < 0
+        ans = 0
+        num = abs(x)
+        while num > 0:
+            ans *= 10
+            ans += num % 10
+            num = num // 10
+        ans = ans * -1 if negative else ans
+        return ans if ans <= (2 ** 31) - 1 and ans >= (-2 ** 31) else 0
