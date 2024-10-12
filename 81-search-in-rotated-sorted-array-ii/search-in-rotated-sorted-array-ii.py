de0081537313741a823dc5a09b68fce9898352eb
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-10-12 11:39:40 -0400

    Time: 53 ms (51.54%) | Memory: 17.6 MB (8.30%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -1,41 +1,34 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> bool:
-        n = len(nums)
-        def binary_search(start, end):
-            left = start
-            right = end
-            while left <= right:
-                mid = (left + right) // 2
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums) - 1
+        def bs(l, r):
+            while l <= r:
+                mid = (l + r) // 2
                 if nums[mid] == target:
                     return True
-                elif nums[mid] < target:
-                    left = mid + 1
+                if nums[mid] > target:
+                    r = mid - 1
                 else:
-                    right = mid - 1
+                    l = mid + 1
             return False
-
-        def find(start, end):
-            if start >= end:
-                if nums[start] == target:
+        def rec(l , r):
+            if l >= r:
+                if nums[l] == target:
                     return True
                 return False
-            middle = (start + end) // 2
-            if nums[middle] == target:
+            mid = (l + r) // 2
+            if nums[mid] == target:
                 return True
-            if nums[middle] < nums[end] and nums[start] > nums[middle]:
-                result = binary_search(middle, end)
-                if result:
-                    return True
-                return find(start, middle - 1)
-            elif nums[middle] > nums[start] and nums[middle] > nums[end]:
-                result = binary_search(start, middle - 1)
-                if  result:
-                    return True
-                return find(middle + 1, end)
+            if nums[mid] > nums[l]:
+                bs_search = bs(l, mid - 1)
+                if bs_search:
+                    return bs_search
+                return rec(mid + 1, r)
+            elif nums[mid] < nums[r]:
+                bs_search = bs(mid + 1 , r)
+                if bs_search:
+                    return bs_search
+                return rec(l , mid - 1)
             else:
-                result_l = find(start, middle - 1)
-                if result_l:
-                    return result_l
-                result_r = find(middle + 1, end)
-                return result_r
-        return find(0, n - 1)
+                return rec(l , mid - 1) or rec(mid + 1, r)
+        return rec(0, n)
