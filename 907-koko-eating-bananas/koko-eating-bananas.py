e50fccaaf7de469cabacd6b40e84ea91cd5e666e
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2024-10-20 22:14:01 -0400

    Time: 135 ms (99.38%) | Memory: 17.7 MB (99.61%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,29 +1,16 @@
 class Solution:
-    
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def brute_force():
-            ans = 0
-            for rate in range(1,high + 1):
-                curr = 0
-                for index in range(len(piles)):
-                    curr += ceil(piles[index] / rate)
-                if curr <= h:
-                    return rate
-        
-        def optimized():
-            low = 1
-            high = max(piles)
-            ans = 0
-            while low <= high:
-                mid = (low + high) // 2
-                curr = 0
-                for pile in piles:
-                    curr += ceil(pile / mid)
-                if curr <= h:
-                    ans = mid
-                    high = mid - 1
-                else:
-                    low = mid + 1
-            return ans
-
-        return optimized()
+        low = 1
+        high = max(piles)
+        ans = high
+        while low <= high:
+            mid = (low + high) // 2
+            curr = 0
+            for pile in piles:
+                curr += ceil(pile / mid)
+            if curr <= h:
+                ans = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        return ans
