1b0e229565610a4fb731f858592f27e7cb64fb62
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2025-07-17 20:50:13 -0400

    Time: 3 ms (65.58%) | Memory: 19.6 MB (77.39%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,17 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        # #1
-        # s = set()
-        # for val in nums:
-        #     if val not in s:
-        #         s.add(val)
-        #     else:
-        #         s.remove(val)
-        # return s.pop()
+        
+        ans = 0
 
-        #2
-        curr = nums[0]
-        for val in range(1, len(nums)):
-            curr = curr ^ nums[val]
+        for val in nums:
 
-        return curr
+            ans = ans ^ val
+
+        return ans
