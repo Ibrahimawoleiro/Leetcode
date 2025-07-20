8554b098bf825e3785eaca8d779b266265c80258
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2025-07-19 23:04:32 -0400

    Time: 0 ms (100.00%) | Memory: 18.4 MB (22.27%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,33 +1,69 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        n = len(nums) - 1
-        def bs(l, r):
-            while l <= r:
-                mid = (l + r) // 2
-                if nums[mid] == target:
-                    return mid
-                if nums[mid] > target:
-                    r = mid - 1
-                else:
-                    l = mid + 1
-            return -1
-        def rec(l , r):
-            if l >= r:
-                if nums[l] == target:
-                    return l
-                return -1
-            mid = (l + r) // 2
-            if nums[mid] == target:
-                return mid
-            if nums[mid] > nums[l]:
-                bs_search = bs(l, mid - 1)
-                if bs_search != -1:
-                    return bs_search
-                return rec(mid + 1, r)
-            else:
-                bs_search = bs(mid + 1 , r)
-                if bs_search != -1:
-                    return bs_search
-                return rec(l , mid - 1)
+        
 
-        return rec(0, n)
+        def bs(left, right):
+
+
+            while left <= right:
+
+                mid = (left + right) // 2
+
+                if nums[mid] == target:
+
+                    return mid
+
+                elif nums[mid] > target:
+
+                    right = mid - 1
+
+                else:
+
+                    left = mid + 1
+            
+            return -1
+
+        low = 0
+
+        high = len(nums) - 1
+
+        def isPresent(l, r):
+
+            if l > r:
+                return -1
+
+            mid = (l + r) // 2
+
+            if nums[mid] == target:
+
+                return mid
+
+            elif nums[mid] >= nums[l]:
+
+                result = bs(l, mid)
+
+                if result != - 1:
+
+                    return result
+
+                result =  isPresent(mid + 1, r)
+
+                if result != - 1:
+                    return result
+
+            elif nums[mid] <= nums[r]:
+
+                result = bs(mid + 1, r)
+
+                if result != - 1:
+
+                    return result
+
+                result =  isPresent(l, mid - 1)
+
+                if result != - 1:
+                    return result
+
+            return -1
+        
+        return isPresent(low, high)
