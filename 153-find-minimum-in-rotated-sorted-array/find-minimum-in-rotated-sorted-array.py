1871a1b33518e055c901c93786d3e1a078986580
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2025-07-19 23:51:39 -0400

    Time: 0 ms (100.00%) | Memory: 18.3 MB (9.41%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,21 +1,24 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        #Identify the minimum side and return the minimum
-        #Identify the unsorted side and call the recursive case on it
-        if len(nums) == 1:
-            return nums[0]
-        n = len(nums)
-        def rec(start, end):
-            if start >= end:
-                if start == end:
-                    return nums[start]
-                return 10000
-            mid = (start + end) // 2
-            if nums[mid] > nums[start]:
-                return min(nums[start], rec(mid + 1, end))
-            elif nums[mid] < nums[end]:
-                return min(nums[mid], rec(start , mid - 1))
-            else:
-                return min(rec(start, mid - 1), rec(mid + 1 , end))
-        result = rec(0 , n - 1)
-        return result 
+        
+        ans = [5001]
+
+        def findMin(low, high):
+            if low > high:
+                return 
+
+            mid = (low + high) // 2
+
+            if nums[mid] >= nums[low]:
+
+                ans[0] = min (ans[0], nums[low])
+
+                return findMin(mid + 1, high)
+
+            ans[0] = min(nums[mid], ans[0])
+
+            return findMin(low, mid - 1)
+
+        findMin(0 , len(nums) - 1)
+
+        return ans[0]
