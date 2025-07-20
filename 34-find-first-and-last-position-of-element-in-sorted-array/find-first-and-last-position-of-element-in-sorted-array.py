3fe7b71f796e927c0c5a37c1025d2816623ed115
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2025-07-19 22:16:39 -0400

    Time: 3 ms (7.42%) | Memory: 19.1 MB (20.59%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,32 +1,62 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums) - 1
-        def find_first_occurrence():
-            ans = -1
+        
+        def first_position():
+
             left = 0
-            right = n
-            while left <= right:
-                mid = (left + right) // 2
-                if nums[mid] == target:
-                    ans = mid
-                    right = mid - 1
-                elif nums[mid] < target:
-                    left = mid + 1
-                else:
-                    right = mid - 1
-            return ans 
-        def find_last_occurrence():
+
+            right = len(nums) - 1
+
             ans = -1
-            left = 0
-            right = n
+
             while left <= right:
+
                 mid = (left + right) // 2
+
                 if nums[mid] == target:
+
                     ans = mid
-                    left = mid + 1
-                elif nums[mid] < target:
-                    left = mid + 1
-                else:
+
                     right = mid - 1
+                
+                elif nums[mid] < target:
+
+                    left = mid + 1
+
+                else:
+
+                    right = mid - 1
+
             return ans
-        return [find_first_occurrence(), find_last_occurrence()]
+
+
+        def last_position():
+
+            left = 0
+
+            right = len(nums) - 1
+
+            ans = -1
+
+            while left <= right:
+
+                mid = (left + right) // 2
+
+                if nums[mid] == target:
+
+                    ans = mid
+
+                    left = mid + 1
+                
+                elif nums[mid] < target:
+
+                    left = mid + 1
+
+                else:
+
+                    right = mid - 1
+
+            return ans
+
+
+        return [first_position(), last_position()]
