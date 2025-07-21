dc98dc0f43b14e4f35946916215a794fb46ae104
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2025-07-20 20:33:06 -0400

    Time: 0 ms (100.00%) | Memory: 24.7 MB (96.53%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,20 +1,52 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        n = len(nums)
+        
+        #We have three cases  on where we land on 
+            #Case 1: We are on the left side of the single element -> If the current set starts from an even index.
+            #Case 2: We are on the right side of the single element -> If the current set starts from an odd index.
+            #We are on the single element -> The single element doesn't match what is on the left or right.
+
+        #Edge Cases:
+            #Case 1: We are on the right side of the single element -> I need to prevent accessing an index that doesn't exist.
+            #Case 2: We are on the element -> I need to prevent accessing an index that doesn't exist.
+        
+        if len(nums) == 1:
+            return nums[0]
+
         left = 0
-        right = n - 1
-        if n == 1:
-            return nums[0]
-        if nums[0] != nums[1]:
-            return nums[0]
-        if nums[right] != nums[right - 1]:
-            return nums[right]
+
+        right = len(nums) - 1
+
         while left <= right:
+
             mid = (left + right) // 2
-            if nums[mid] != nums[mid - 1] and nums[mid] != nums[mid + 1]:
-                return nums[mid] 
-            elif (nums[mid] == nums[mid + 1] and mid % 2 == 0) or (nums[mid] == nums[mid - 1] and mid % 2 == 1): 
-                left = mid + 1
+
+            if mid % 2 == 0 :
+
+                if mid != len(nums) - 1 and nums[mid] == nums[mid + 1]:
+
+                    left = mid + 2
+
+                elif mid != 0 and  nums[mid] == nums[mid - 1]:
+
+                    right = mid - 1
+
+                else:
+
+                    return nums[mid]
+
             else:
-                right = mid - 1
-        return -1
+
+                if  nums[mid] == nums[mid - 1]:
+
+                    left = mid + 1
+
+                elif nums[mid] == nums[mid + 1]:
+
+                    right = mid - 1
+                
+                else:
+
+                    return nums[mid]
+
+                
