c285d440a8203cc170aca09e139c988e44a447aa
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2025-07-21 23:05:10 -0400

    Time: 313 ms (54.12%) | Memory: 31.3 MB (98.68%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
@@ -1,44 +1,46 @@
 class Solution:
     def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
         
-        def brute_force():
-            low = 1
-            high = max(bloomDay)
-            for days in (range(1, high + 1)):
-                bouquet = 0
-                flowers = 0
-                for flower_index in range(len(bloomDay)):
-                    if bloomDay[flower_index] <= days:
-                        flowers += 1
-                        if flowers == k:
-                            bouquet += 1
-                            flowers = 0
-                    else:
-                        flowers = 0
-                if bouquet == m:
-                    return days
-            return -1
-        def optimized():
-            low = 1
-            high = max(bloomDay)
-            ans = -1
-            while low <= high:
-                mid = (low + high) // 2
-                bouquet = 0
-                flowers = 0
-                for flower_bloom in bloomDay:
-                    if  flower_bloom <= mid:
-                        flowers += 1
-                        if flowers == k:
-                            bouquet += 1
-                            flowers = 0
-                    else:
-                        flowers = 0
-                if bouquet >= m:
-                    ans = mid
-                    high = mid - 1
-                else:
-                    low = mid + 1
+        low = 1
 
-            return ans
-        return optimized()
+        high = max(bloomDay)
+
+        ans = -1
+
+        while low <= high:
+
+            mid = (low + high) // 2
+
+            bouquets = 0
+
+            adj_count = 0
+
+            for val in bloomDay:
+
+                if val <= mid:
+
+                    adj_count += 1
+
+                else:
+
+                    adj_count = 0
+
+                if adj_count == k:
+
+                    bouquets += 1
+
+                    adj_count = 0
+
+            if bouquets >= m:
+
+                ans = mid
+
+                high = mid - 1
+            
+            else:
+
+                low = mid + 1
+
+        
+        return ans
+                
