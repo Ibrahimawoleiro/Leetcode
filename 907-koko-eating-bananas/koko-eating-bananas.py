6c5a189e0d7561da6f4829bc9b32d1432cf3ca7b
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2025-07-21 22:33:02 -0400

    Time: 159 ms (51.85%) | Memory: 18.9 MB (47.85%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,16 +1,35 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
         low = 1
+
         high = max(piles)
-        ans = high
+
+        ans = 0
+
+        total = float('inf')
+
         while low <= high:
+
             mid = (low + high) // 2
-            curr = 0
-            for pile in piles:
-                curr += ceil(pile / mid)
-            if curr <= h:
+
+            curr_total = 0
+
+            for val in piles:
+
+                curr_total += math.ceil((val / mid))
+
+            if curr_total <= h:
+
                 ans = mid
+
+                total = curr_total
+
                 high = mid - 1
-            else:
+            
+            else: 
+
                 low = mid + 1
-        return ans
+
+        return ans 
+
