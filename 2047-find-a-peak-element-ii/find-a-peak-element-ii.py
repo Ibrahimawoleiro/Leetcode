6c973966cd72fd697d7696cdd3afbc789d6f6b4b
Ibrahimawoleiro <96323766+Ibrahimawoleiro@users.noreply.github.com>
2025-08-30 15:45:11 -0400

    Time: 2 ms (26.60%) | Memory: 46.2 MB (21.27%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.py
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.py
@@ -1,23 +1,61 @@
 class Solution:
     def findPeakGrid(self, mat: List[List[int]]) -> List[int]:
-        low = 0
-        high = len(mat) - 1
-        while low <= high:
-            mid = (low + high) // 2
-            max_col = 0
-            val = mat[mid][0]
-            for index in range(len(mat[0])):
-                if mat[mid][index] > val:
-                    val = mat[mid][index]
-                    max_col = index
-            top = True
-            bottom = True
-            if mid - 1 >= 0 and mat[mid - 1][max_col] > mat[mid][max_col]:
-                high = mid - 1
-                top = False
-            if mid + 1 < len(mat) and mat[mid + 1][max_col] > mat[mid][max_col]:
-                low = mid + 1
-                bottom = False
-            if top and bottom:
-                return [mid, max_col]
-        return [-1, -1]
+        
+        current_row = 0
+        current_col = len(mat[0]) - 1
+
+        def isPeak(row, col, current_val):
+
+            upper_neighbor = mat[row - 1][col] if row > 0 else -1
+            bottom_neighbor = mat[row + 1][col] if row < len(mat) - 1 else -1
+            left_neighbor = mat[row][col - 1] if col > 0 else -1
+            right_neighbor = mat[row][col + 1] if col < len(mat[0]) - 1 else -1
+
+            if current_val > upper_neighbor and current_val > bottom_neighbor and current_val > left_neighbor and current_val > right_neighbor: 
+
+                return True
+
+            return False
+
+        def max_neighbor(row, col):
+
+            current = mat[row][col]
+
+            upper_neighbor = mat[row - 1][col] if row > 0 else -1
+            bottom_neighbor = mat[row + 1][col] if row < len(mat) - 1 else -1
+            left_neighbor = mat[row][col - 1] if col > 0 else -1
+            right_neighbor = mat[row][col + 1] if col < len(mat[0]) - 1 else -1
+
+            max_sibling = max(upper_neighbor, max(bottom_neighbor, max(left_neighbor, right_neighbor)))
+
+            if max_sibling == upper_neighbor:
+                return [row - 1,col]
+
+            elif max_sibling == bottom_neighbor:
+                return [row + 1,col]
+
+            elif max_sibling == left_neighbor:
+                return [row,col - 1]
+
+            else:
+                return [row,col + 1]
+
+
+
+
+        while current_row > -1 and current_row < len(mat) and current_col > -1 and current_col < len(mat[0]):
+
+            current_value = mat[current_row][current_col]
+
+            if isPeak(current_row, current_col, current_value):
+                print(current_row, current_col)
+                return [current_row,current_col]
+
+            next_position = max_neighbor(current_row, current_col)
+
+            current_row = next_position[0]
+            current_col = next_position[1]
+
+        return [-1,-1]
+
+            
